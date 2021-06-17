1b95a8ccff74e53a25eea438346d9985e5b450fd
Victor Rentea <victorrentea@gmail.com>
2021-06-17 13:10:45 +0300

    App

--- clean-application/src/main/java/victor/training/clean/Application.java
+++ clean-application/src/main/java/victor/training/clean/Application.java
@@ -1,11 +1,13 @@
 package victor.training.clean;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
+import victor.training.clean.service.UserService;
 
 @EnableAsync
 @SpringBootApplication
@@ -19,6 +21,12 @@ public class Application {
 
    public static void main(String[] args) {
       SpringApplication.run(Application.class, args);
+   }
+
+   @Autowired
+   private UserService userService;
+   public void someUsage() {
+      userService.importUserFromLdap("a");
    }
 
 }
