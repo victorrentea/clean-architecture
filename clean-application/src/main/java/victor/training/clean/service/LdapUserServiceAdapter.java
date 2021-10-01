5cdf46aa0f5da850ca4546499c0bc22abab98745
Victor Rentea <victorrentea@gmail.com>
2021-10-01 10:49:25 +0300

    Am introdus Adapter pentru a ma apara de DTO externe: AntiCorruption

---
+++ clean-application/src/main/java/victor/training/clean/service/LdapUserServiceAdapter.java
@@ -0,0 +1,30 @@
+package victor.training.clean.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import victor.training.clean.entity.User;
+import victor.training.clean.infra.LdapUserDto;
+import victor.training.clean.infra.LdapUserWebserviceClient;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class LdapUserServiceAdapter {
+   private final LdapUserWebserviceClient client;
+
+   public List<User> searchByUsername(String username) {
+      return client.search(username.toUpperCase(), null, null)
+          .stream()
+          .map(this::fromDto)
+          .collect(Collectors.toList());
+   }
+
+   private User fromDto(LdapUserDto dto) {
+      String fullName = dto.getfName() + " " + dto.getlName().toUpperCase();
+      return new User(dto.getuId(), fullName, dto.getWorkEmail());
+   }
+
+
+}
--- clean-application/src/main/java/victor/training/clean/service/UserService.java
+++ clean-application/src/main/java/victor/training/clean/service/UserService.java
@@ -1,42 +1,37 @@
 package victor.training.clean.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import victor.training.clean.entity.User;
 import victor.training.clean.infra.LdapUserDto;
 import victor.training.clean.infra.LdapUserWebserviceClient;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class UserService {
-	@Autowired
-	private LdapUserWebserviceClient wsClient;
+	private final LdapUserServiceAdapter adapter;
 
 	public void importUserFromLdap(String username) {
-		List<LdapUserDto> list = searchByUsername(username);
+		List<User> list = adapter.searchByUsername(username);
 		if (list.size() != 1) {
 			throw new IllegalArgumentException("There is no single user matching username " + username);
 		}
-		User user = fromDto(list.get(0));
+		User user = list.get(0);
 
 		if (user.getWorkEmail() != null) {
 			log.debug("Send welcome email to " + user.getWorkEmail());
 		}
 		log.debug("Insert user in my database");
 	}
-
-	private User fromDto(LdapUserDto dto) {
-		String fullName = dto.getfName() + " " + dto.getlName().toUpperCase();
-		return new User(dto.getuId(), fullName, dto.getWorkEmail());
-	}
-
-
-	private List<LdapUserDto> searchByUsername(String username) {
-		return wsClient.search(username.toUpperCase(), null, null);
-	}
-
+// --- deasupra liniei e sufrageria
+	///-----------
+	// buda
 
 }
+
