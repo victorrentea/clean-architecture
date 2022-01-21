e36426ae7b2289174d315d3803bbf664855ed4a5
Victor Rentea <victorrentea@gmail.com>
2022-01-21 12:41:41 +0200

    immutable DTOs :), finally

--- clean-application/src/main/java/victor/training/clean/facade/dto/CustomerSearchCriteria.java
+++ clean-application/src/main/java/victor/training/clean/facade/dto/CustomerSearchCriteria.java
@@ -1,6 +1,9 @@
 package victor.training.clean.facade.dto;
 
+import lombok.Value;
+
 // received as JSON from search form in Frontend
+@Value // see lombok.config for details how Jackson knows to unmarshall this
 public class CustomerSearchCriteria {
-   public String name;
+   String name;
 }
