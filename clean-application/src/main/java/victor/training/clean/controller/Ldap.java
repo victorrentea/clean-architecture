47677bb82ee63d1921bd940e76344be48e08e718
Victor Rentea <victorrentea@gmail.com>
2022-02-04 00:28:20 +0200

    restore controller

--- clean-application/src/main/java/victor/training/clean/controller/Ldap.java
+++ clean-application/src/main/java/victor/training/clean/controller/Ldap.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+//@RestController
 public class Ldap {
 
    @GetMapping("ldap/user")
