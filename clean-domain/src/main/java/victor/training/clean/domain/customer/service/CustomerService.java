704e0c03d4e2c2d49ad033c3462329cc8aeed107
victorrentea <victorrentea@gmail.com>
2022-06-08 12:49:29 +0300

    block the access to repos of other subdomains

---
+++ clean-domain/src/main/java/victor/training/clean/domain/customer/service/CustomerService.java
@@ -0,0 +1,17 @@
+package victor.training.clean.domain.customer.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import victor.training.clean.domain.customer.model.Customer;
+import victor.training.clean.domain.customer.repo.CustomerRepo;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+    private final CustomerRepo customerRepo;
+    public Customer findById(Long customerId) {
+        return customerRepo.findById(customerId).orElseThrow();
+    }
+}
--- clean-domain/src/main/java/victor/training/clean/domain/insurance/service/QuotationService.java
+++ clean-domain/src/main/java/victor/training/clean/domain/insurance/service/QuotationService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import victor.training.clean.domain.customer.model.Customer;
+import victor.training.clean.domain.customer.repo.CustomerRepo;
+import victor.training.clean.domain.customer.service.CustomerService;
 import victor.training.clean.domain.insurance.model.InsurancePolicy;
 import victor.training.clean.domain.insurance.repo.InsurancePolicyRepo;
 
@@ -14,12 +16,20 @@ import java.math.BigDecimal;
 @RequiredArgsConstructor
 public class QuotationService {
    private final InsurancePolicyRepo insurancePolicyRepo;
+   private final CustomerService customerService;
 
    public void quoteCustomer(Customer customer) {
       log.debug("Quoting customer (~230 total lines of code, 40 Cyclomatic Complexity): " + customer.getName());
       InsurancePolicy policy = new InsurancePolicy();
-      policy.setCustomer(customer);
+      policy.setCustomerId(customer.getId());
       policy.setValueInEur(BigDecimal.ONE);
       insurancePolicyRepo.save(policy);
+   }
+
+   public void printPolicy(Long policyId) {
+      InsurancePolicy policy = insurancePolicyRepo.findById(policyId).orElseThrow();
+      Customer customer = customerService.findById(policy.getCustomerId());
+      System.out.println(customer.getName());
+
    }
 }
