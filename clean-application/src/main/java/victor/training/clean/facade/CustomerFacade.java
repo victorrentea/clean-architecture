d7949b19d4ff2f86791a15796e38f10c8a64b909
Victor Rentea <victorrentea@gmail.com>
2022-10-10 15:48:39 +0300

    prep

--- clean-application/src/main/java/victor/training/clean/facade/CustomerFacade.java
+++ clean-application/src/main/java/victor/training/clean/facade/CustomerFacade.java
@@ -19,7 +19,7 @@ import java.util.List;
 
 //@Service
 @Facade
-@Transactional
+@Transactional // probably too broad for high-TPS systems
 @RequiredArgsConstructor
 public class CustomerFacade {
     private final CustomerRepo customerRepo;
