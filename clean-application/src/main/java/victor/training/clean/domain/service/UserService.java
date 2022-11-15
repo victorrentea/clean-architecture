8a742c9c6cf45b5905bb7c2d88ef0d3ec2b00f88
Victor Rentea <victorrentea@gmail.com>
2022-11-15 10:41:05 +0200

    moved away the adapter into the infrastructure

--- clean-application/src/main/java/victor/training/clean/domain/service/UserService.java
+++ clean-application/src/main/java/victor/training/clean/domain/service/UserService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import victor.training.clean.domain.model.User;
+import victor.training.clean.infra.LdapApiAdapter;
 
 @RequiredArgsConstructor
 @Slf4j
