0d80c79a4c7a533325ff531694445b557d8e07bf
Victor Rentea <victorrentea@gmail.com>
2022-11-21 14:45:00 +0200

    Dependency Inversion enforced via compiler by putting the domain in a differnt module

--- clean-application/src/main/java/victor/training/clean/domain/repo/SiteRepo.java
+++
@@ -1,7 +0,0 @@
-package victor.training.clean.domain.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import victor.training.clean.domain.model.Site;
-
-public interface SiteRepo extends JpaRepository<Site, Long> {
-}
