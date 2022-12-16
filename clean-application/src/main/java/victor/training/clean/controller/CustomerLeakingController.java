18e491254e7df8d1ce4b3fd3d72c81f3b0adb136
Victor Rentea <victorrentea@gmail.com>
2022-12-16 12:42:22 +0200

    arch unit ftw

--- clean-application/src/main/java/victor/training/clean/controller/CustomerLeakingController.java
+++ clean-application/src/main/java/victor/training/clean/controller/CustomerLeakingController.java
@@ -1,25 +1,25 @@
-package victor.training.clean.controller;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import victor.training.clean.domain.model.Customer;
-import victor.training.clean.domain.repo.CustomerRepo;
-
-@RestController
-@RequestMapping("customerLeaking")
-@RequiredArgsConstructor
-public class CustomerLeakingController {
-   private final CustomerRepo customerRepo;
-
-   @GetMapping("{id}")
-   public Customer findById(@PathVariable long id) {
-      return customerRepo.findById(id).orElseThrow();
-   }
-
-   @GetMapping("{id}/2")
-   public ResponseEntity<Customer> findByIdEntity(@PathVariable long id) {
-      return ResponseEntity.ok(customerRepo.findById(id).orElseThrow());
-   }
-
-}
+//package victor.training.clean.controller;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//import victor.training.clean.domain.model.Customer;
+//import victor.training.clean.domain.repo.CustomerRepo;
+//
+//@RestController
+//@RequestMapping("customerLeaking")
+//@RequiredArgsConstructor
+//public class CustomerLeakingController {
+//   private final CustomerRepo customerRepo;
+//
+//   @GetMapping("{id}")
+//   public Customer findById(@PathVariable long id) {
+//      return customerRepo.findById(id).orElseThrow();
+//   }
+//
+//   @GetMapping("{id}/2")
+//   public ResponseEntity<Customer> findByIdEntity(@PathVariable long id) {
+//      return ResponseEntity.ok(customerRepo.findById(id).orElseThrow());
+//   }
+//
+//}
