1df7f2d4f28150bcfa4935cb17552f91ae95f244
Victor Rentea <victorrentea@gmail.com>
2023-03-07 18:16:06 +0200

    add ctor to dto

--- clean-application/src/main/java/victor/training/clean/application/dto/CustomerDto.java
+++ clean-application/src/main/java/victor/training/clean/application/dto/CustomerDto.java
@@ -4,8 +4,12 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Value;
+import victor.training.clean.domain.model.Customer;
 
 import javax.validation.constraints.Size;
+import java.time.format.DateTimeFormatter;
+
+import static java.time.format.DateTimeFormatter.ofPattern;
 
 @Builder
 @Value
@@ -20,4 +24,14 @@ public class CustomerDto { // Dto used to both QUERY and COMMAND use-cases ?
   String creationDateStr;
   boolean gold;
   String goldMemberRemovalReason;
+
+  public CustomerDto(Customer customer) {
+    id = customer.getId();
+    name = customer.getName();
+    email = customer.getEmail();
+    siteId = customer.getSite().getId();
+    creationDateStr = customer.getCreationDate().format(ofPattern("yyyy-MM-dd"));
+    gold = customer.isGoldMember();
+    goldMemberRemovalReason = "TODO";
+  }
 }
