068a9c2a840974c89ae7d715d51d36ea0a06d91c
Victor Rentea <victorrentea@gmail.com>
2023-03-09 01:10:28 +0200

    Rest Repository

--- clean-application/src/main/java/victor/training/clean/verticalslice/CountryCRUD.java
+++ clean-application/src/main/java/victor/training/clean/verticalslice/CountryCRUD.java
@@ -1,12 +1,18 @@
 package victor.training.clean.verticalslice;
 
 import lombok.*;
+import org.springdoc.core.providers.RepositoryRestConfigurationProvider;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.mapping.RepositoryDetectionStrategy;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,9 +20,17 @@ import javax.persistence.Id;
 import java.util.List;
 
 // Brutal example of vertical slicing to enable flexible architecture per use-case: here- the lack of it :)
-@RepositoryRestResource(collectionResourceRel = "countries", path = "countries")
+@RepositoryRestResource
 public interface CountryCRUD extends PagingAndSortingRepository<Country, Long> {
     List<Country> findByName(@Param("name") String name);
+}
+
+@Component
+class DontAutoExposeRepos implements RepositoryRestConfigurer {
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.setRepositoryDetectionStrategy(RepositoryDetectionStrategy.RepositoryDetectionStrategies.ANNOTATED);
+        // config.setExposeRepositoryMethodsByDefault(false);
+    }
 }
 @Entity
 @Getter
