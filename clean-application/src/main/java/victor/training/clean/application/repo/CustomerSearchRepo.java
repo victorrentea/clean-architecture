956fc5752c4a1d450da05ba526a12e7972d73f7c
Victor Rentea <victorrentea@gmail.com>
2023-05-28 23:40:16 +0300

    comment criteria API

--- clean-application/src/main/java/victor/training/clean/application/repo/CustomerSearchRepo.java
+++ clean-application/src/main/java/victor/training/clean/application/repo/CustomerSearchRepo.java
@@ -1,21 +1,11 @@
 package victor.training.clean.application.repo;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Repository;
 import victor.training.clean.application.dto.CustomerSearchCriteria;
 import victor.training.clean.application.dto.CustomerSearchResult;
-import victor.training.clean.domain.model.Country_;
-import victor.training.clean.domain.model.Customer;
-import victor.training.clean.domain.model.Customer_;
-import victor.training.clean.domain.repo.CustomerRepo;
 
 import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -61,30 +51,35 @@ public class CustomerSearchRepo {
    }
 
    //region CriteriaAPI alternative
-   public List<CustomerSearchResult> searchWithCriteria(CustomerSearchCriteria criteria) {
-      CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-      CriteriaQuery<CustomerSearchResult> criteriaQuery = cb.createQuery(CustomerSearchResult.class);
-      Root<Customer> root = criteriaQuery.from(Customer.class);
-      criteriaQuery.select(cb.construct(CustomerSearchResult.class, root.get(Customer_.id), root.get(Customer_.name)));
-      List<Predicate> predicates = new ArrayList<>();
-      predicates.add(cb.isTrue(cb.literal(true)));
-
-      if (criteria.getName() != null) {
-         predicates.add(cb.like(cb.upper(root.get(Customer_.name)), cb.upper(cb.literal("%" + criteria.getName() + "%"))));
-      }
-
-      if (criteria.getEmail() != null) {
-         predicates.add(cb.equal(cb.upper(root.get(Customer_.email)), cb.upper(cb.literal(criteria.getEmail()))));
-      }
-
-      if (criteria.getCountryId() != null) {
-         predicates.add(cb.equal(root.get(Customer_.country).get(Country_.id), criteria.getCountryId()));
-      }
-      criteriaQuery.where(cb.and(predicates.toArray(new Predicate[0])));
-
-      TypedQuery<CustomerSearchResult> query = entityManager.createQuery(criteriaQuery);
-      return query.getResultList();
-   }
+   // add to pom <dependency>
+   //            <!--Generate Criteria API metamodel in target/generated-sources/annotations-->
+   //            <groupId>org.hibernate</groupId>
+   //            <artifactId>hibernate-jpamodelgen</artifactId>
+   //        </dependency>
+//   public List<CustomerSearchResult> searchWithCriteria(CustomerSearchCriteria criteria) {
+//      CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+//      CriteriaQuery<CustomerSearchResult> criteriaQuery = cb.createQuery(CustomerSearchResult.class);
+//      Root<Customer> root = criteriaQuery.from(Customer.class);
+//      criteriaQuery.select(cb.construct(CustomerSearchResult.class, root.get(Customer_.id), root.get(Customer_.name)));
+//      List<Predicate> predicates = new ArrayList<>();
+//      predicates.add(cb.isTrue(cb.literal(true)));
+//
+//      if (criteria.getName() != null) {
+//         predicates.add(cb.like(cb.upper(root.get(Customer_.name)), cb.upper(cb.literal("%" + criteria.getName() + "%"))));
+//      }
+//
+//      if (criteria.getEmail() != null) {
+//         predicates.add(cb.equal(cb.upper(root.get(Customer_.email)), cb.upper(cb.literal(criteria.getEmail()))));
+//      }
+//
+//      if (criteria.getCountryId() != null) {
+//         predicates.add(cb.equal(root.get(Customer_.country).get(Country_.id), criteria.getCountryId()));
+//      }
+//      criteriaQuery.where(cb.and(predicates.toArray(new Predicate[0])));
+//
+//      TypedQuery<CustomerSearchResult> query = entityManager.createQuery(criteriaQuery);
+//      return query.getResultList();
+//   }
    //endregion
 
 }
