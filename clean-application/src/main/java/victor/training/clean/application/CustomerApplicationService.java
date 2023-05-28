912d7c0a11b7b8ab387c1cffeee0da511aa5cadf
Victor Rentea <victorrentea@gmail.com>
2023-05-29 00:17:51 +0300

    yolo

--- clean-application/src/main/java/victor/training/clean/application/CustomerApplicationService.java
+++ clean-application/src/main/java/victor/training/clean/application/CustomerApplicationService.java
@@ -2,6 +2,8 @@ package victor.training.clean.application;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.transaction.annotation.Transactional;
 import victor.training.clean.common.ApplicationService;
 import victor.training.clean.domain.model.*;
@@ -20,9 +22,9 @@ import java.util.List;
 
 import static java.util.Objects.requireNonNull;
 
-@Slf4j
+@Slf4j // ❤️Lombok adds private static final Logger log = LoggerFactory.getLogger(CustomerApplicationService.class);
+@RequiredArgsConstructor // ❤️Lombok generates constructor including all 'private final' fields
 @ApplicationService // custom annotation refining the classic @Service
-@RequiredArgsConstructor // generates constructor for all 'private final' fields
 public class CustomerApplicationService {
     private final CustomerRepo customerRepo;
     private final EmailSender emailSender;
