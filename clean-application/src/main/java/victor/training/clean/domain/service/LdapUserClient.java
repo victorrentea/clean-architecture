1b2f7869e4264463ac430cb8d8a266b39aacc307
Victor Rentea <victorrentea@gmail.com>
2023-05-29 13:59:31 +0300

    9/10: extracted the conversion + call to outside into an Adapter ("...Client")

---
+++ clean-application/src/main/java/victor/training/clean/domain/service/LdapUserClient.java
@@ -0,0 +1,45 @@
+package victor.training.clean.domain.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import victor.training.clean.common.Adapter;
+import victor.training.clean.domain.model.User;
+import victor.training.clean.infra.LdapApi;
+import victor.training.clean.infra.LdapUserDto;
+
+import java.util.List;
+
+// Adapter design pattern in GoF
+//@Component
+@RequiredArgsConstructor
+@Adapter
+public class LdapUserClient {
+  private final LdapApi ldapApi;
+
+  public User fetchByUsername(String targetUsername) {
+    List<LdapUserDto> dtoList = ldapApi.searchUsingGET(null, null, targetUsername.toUpperCase());
+
+    if (dtoList.size() != 1) {
+      throw new IllegalArgumentException("Expected single user to match username " + targetUsername + ", got: " + dtoList);
+    }
+    LdapUserDto dto = dtoList.get(0);
+
+    User user = fromDto(dto);
+    return user;
+  }
+
+  private static User fromDto(LdapUserDto dto) {
+//    String username = dto.getUid();
+//    if (username == null) {
+//      username = "anonymous";
+//    }
+
+//    String username = Optional.ofNullable(dto.getUid()).orElse("anonymous");
+    String username = dto.getUid() != null ? dto.getUid() : "anonymous";
+
+    String fullName = dto.getFname() + " " + dto.getLname().toUpperCase();
+    String workEmail = dto.getWorkEmail() != null ? dto.getWorkEmail().toLowerCase():null;
+    return new User(username, fullName, workEmail);
+  }
+
+}
--- clean-application/src/main/java/victor/training/clean/domain/service/UserService.java
+++ clean-application/src/main/java/victor/training/clean/domain/service/UserService.java
@@ -4,48 +4,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import victor.training.clean.domain.model.User;
-import victor.training.clean.infra.LdapApi;
-import victor.training.clean.infra.LdapUserDto;
-
-import java.util.List;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Slf4j
 @Service
 public class UserService {
-  private final LdapApi ldapApi;
+  private final LdapUserClient ldapUserClient;
 
   public void importUserFromLdap(String targetUsername) {
-    List<LdapUserDto> dtoList = ldapApi.searchUsingGET(null, null, targetUsername.toUpperCase());
-
-    if (dtoList.size() != 1) {
-      throw new IllegalArgumentException("Expected single user to match username " + targetUsername + ", got: " + dtoList);
-    }
-
-    LdapUserDto dto = dtoList.get(0);
-
-//    String username = dto.getUid();
-//    if (username == null) {
-//      username = "anonymous";
-//    }
-
-//    String username = Optional.ofNullable(dto.getUid()).orElse("anonymous");
-
-    User user = fromDto(dto);
-    complexLogic(user);
-  }
-
-  private static User fromDto(LdapUserDto dto) {
-    String username = dto.getUid() != null ? dto.getUid() : "anonymous";
-
-    String fullName = dto.getFname() + " " + dto.getLname().toUpperCase();
-    String workEmail = dto.getWorkEmail() != null ? dto.getWorkEmail().toLowerCase():null;
-    User user = new User(username, fullName, workEmail);
-    return user;
-  }
-
-  private void complexLogic(User user) {
+    User user = ldapUserClient.fetchByUsername(targetUsername);
     user.getEmail().ifPresent(this::checkNewUser);
 
     log.debug("Insert user in my database: " + user.getUsername());
