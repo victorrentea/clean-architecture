9f8c4d84fc60260c182b9a0fffe5b429b1f0bf10
Victor Rentea <victorrentea@gmail.com>
2023-06-12 12:42:39 +0300

    domain vorbeste cu exteriorul printr-un adepter pe care nu-l vede ca implementare

--- clean-application/src/main/java/victor/training/clean/domain/service/UserService.java
+++ clean-application/src/main/java/victor/training/clean/domain/service/UserService.java
@@ -4,30 +4,20 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import victor.training.clean.domain.model.User;
-import victor.training.clean.infra.LdapApi;
-import victor.training.clean.infra.LdapUserDto;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @Slf4j
 @Service
 public class UserService {
-  private final LdapApi ldapApi;
+  private final UserProvider userProvider;
 
   public void importUserFromLdap(String targetUsername) {
-    List<LdapUserDto> dtoList = ldapApi.searchUsingGET(null, null, targetUsername.toUpperCase());
-
-    if (dtoList.size() != 1) {
-      throw new IllegalArgumentException("Expected single user to match username " + targetUsername + ", got: " + dtoList);
-    }
-
-    LdapUserDto dto = dtoList.get(0);
-    String fullName = dto.getFname() + " " + dto.getLname().toUpperCase();
-    User user = new User(dto.getUid(), dto.getWorkEmail(), fullName);
+    User user = userProvider.getUserByUsername(targetUsername);
 
     complexLogic(user);
   }
+
+
 
   private void complexLogic(User user) {
     user.getEmail().ifPresent(this::checkNewUser);
---
+++ clean-application/src/main/java/victor/training/clean/infra/LdapUserProvider.java
@@ -0,0 +1,34 @@
+package victor.training.clean.infra;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import victor.training.clean.common.Adapter;
+import victor.training.clean.domain.model.User;
+import victor.training.clean.domain.service.UserProvider;
+import victor.training.clean.infra.LdapApi;
+import victor.training.clean.infra.LdapUserDto;
+
+import java.util.List;
+
+@Component
+public class LdapUserProvider implements UserProvider {
+  @Autowired
+  private LdapApi ldapApi;
+
+  @Override
+  public User getUserByUsername(String targetUsername) {
+    List<LdapUserDto> dtoList = ldapApi.searchUsingGET(null, null, targetUsername.toUpperCase());
+
+    if (dtoList.size() != 1) {
+      throw new IllegalArgumentException("Expected single user to match username " + targetUsername + ", got: " + dtoList);
+    }
+
+    return fromDto(dtoList);
+  }
+
+  private static User fromDto(List<LdapUserDto> dtoList) {
+    LdapUserDto dto = dtoList.get(0);
+    String fullName = dto.getFname() + " " + dto.getLname().toUpperCase();
+    return new User(dto.getUid(), dto.getWorkEmail(), fullName);
+  }
+}
