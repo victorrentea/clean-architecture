dddb288a0c63d9d589717811a3a97f6a5b6484ad
Victor Rentea <victorrentea@gmail.com>
2023-09-06 13:09:13 +0300

    used javac to enforce Dep Inversion by 2 maven modulels. Infra ---> Domain

--- clean-application/src/main/java/victor/training/clean/domain/service/ExternalUserProvider.java
+++
@@ -1,8 +0,0 @@
-package victor.training.clean.domain.service;
-
-import victor.training.clean.domain.model.User;
-
-public interface ExternalUserProvider {
-  // the public api of the adapter does not LEAK external concepts (eg LdapUserDto). Only uses my domain objects!!! (or primitives)
-  User loadUserFromLdap(String userId);
-}
