98e3007b60e46f0c320531aee08a4ec6c62026d7
Victor Rentea <victorrentea@gmail.com>
2023-11-09 11:34:12 +0200

    spargem in substructuri Domain Model. @Embeddable modelu devine mai 'suplu' mai mici clasele, dar mai 'adanc'

--- clean-application/src/main/java/victor/training/clean/domain/model/Customer.java
+++ clean-application/src/main/java/victor/training/clean/domain/model/Customer.java
@@ -1,18 +1,38 @@
 package victor.training.clean.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import java.time.LocalDate;
 
 import static java.util.Objects.requireNonNull;
 
+//(imuutable❤️)
+//record in Hib 6.2
+@Embeddable
+@Getter
+class ShippingAddress {
+  private String city;
+  private String street;
+  private Integer zipCode;
+
+  protected ShippingAddress() {} // for Hibernate only
+
+  public ShippingAddress(String city, String street, Integer zipCode) {
+    this.city = city;
+    this.street = street;
+    this.zipCode = zipCode;
+  }
+}
+
 @Entity
-@Data // BAD because: 1) hashCode on @Id, 2) toString can trigger lazy-loading, 3) all setters = no encapsulation
+@Data // BAD because:
+// 1) hashCode on @Id,
+// 2) toString can trigger lazy-loading,
+// 3) all setters = no encapsulation, de fapt programezi can C++ cu struct
+// instead: use @Getter on class + @Setter pe unele campuri
 public class Customer {
   @Id
   @GeneratedValue
@@ -21,9 +41,11 @@ public class Customer {
   private String email;
 
   // 🤔 Hmm... 3 fields with the same prefix. What TODO ?
-  private String shippingAddressCity;
-  private String shippingAddressStreet;
-  private Integer shippingAddressZipCode;
+//  private String shippingAddressCity;
+//  private String shippingAddressStreet;
+//  private Integer shippingAddressZipCode;
+  @Embedded // cele 3 coloane RAMAN in tabela CUSTOMER
+  private ShippingAddress shippingAddress;
 
   @ManyToOne
   private Country country;
