81651f94af6f15bff950ce6e9b7e334e916c641d
Victor Rentea <victorrentea@gmail.com>
2023-11-10 11:44:26 +0200

    Revert "VSA - vertical Slice Architecture"
    This reverts commit b71d9480c566a797aafb2fbf8147c6d87d3b52fd.

--- clean-application/src/main/java/victor/training/clean/verticalslice/CountryRestRepo.java
+++ clean-application/src/main/java/victor/training/clean/verticalslice/CountryRestRepo.java
@@ -1,4 +1,4 @@
-package victor.training.clean.vsa;
+package victor.training.clean.verticalslice;
 
 import lombok.*;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
--- clean-application/src/main/java/victor/training/clean/verticalslice/SearchCustomerUseCase.java
+++ clean-application/src/main/java/victor/training/clean/verticalslice/SearchCustomerUseCase.java
@@ -1,14 +1,13 @@
-package victor.training.clean.vsa;
+package victor.training.clean.verticalslice;
 
-import com.google.common.annotations.VisibleForTesting;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import jakarta.persistence.EntityManager;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,30 +17,28 @@ import static java.lang.String.join;
 
 @SuppressWarnings("JpaQlInspection")
 @RequiredArgsConstructor
-@RestController
-// spargi mai tarziu class PlaceOrderUseCase dacat OrderService.placeOrder()
+//@RestController
 public class SearchCustomerUseCase {
   private final EntityManager entityManager;
 
-  @VisibleForTesting
-  record SearchCustomerRequest( // JSON
-      String name,
-      String email,
-      Long countryId
-  ) {  }
-
-  record SearchCustomerResponse( // JSON
-      long id,
-      String name,
-      // TODO if we add 'email' to results => only this file is impacted
-      String email
-  ) { }
+  @Value
+  // in/out structures kept private, inaccessible from other Use-Cases!
+  public static class SearchCustomerRequest { // JSON
+    String name;
+    String email;
+    Long countryId;
+  }
+  @Value
+  public static class SearchCustomerResponse { // JSON
+    long id;
+    String name;
+    // TODO if we add 'email' to results => only this file is impacted
+  }
 
   @Operation(description = "Customer Search")
   @PostMapping("customer/search")
   public List<SearchCustomerResponse> search(@RequestBody SearchCustomerRequest criteria) {
-    String jpql = "SELECT new victor.training.clean.vsa.SearchCustomerUseCase$SearchCustomerResponse(" +
-                  " c.id, c.name, c.email)" +
+    String jpql = "SELECT new victor.training.clean.verticalslice.SearchCustomerUseCase$SearchCustomerResponse(c.id, c.name)" +
                   " FROM Customer c " +
                   " WHERE ";
     List<String> jpqlParts = new ArrayList<>();
