0e37784b020402fa04993b26be3c574a5c158cce
Victor Rentea <victorrentea@gmail.com>
2023-11-10 11:51:19 +0200

    undo

--- clean-application/src/main/java/victor/training/clean/infra/db/entity/Country.java
+++ clean-application/src/main/java/victor/training/clean/infra/db/entity/Country.java
@@ -1,26 +1,26 @@
-
-package victor.training.clean.infra.db.entity;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.Data;
-
-@Entity
-@Data
-public class Country {
-  @Id
-  @GeneratedValue
-  private long id;
-  private String name;
-  private String iso;
-
-  public Country() {
-  }
-
-  public Country(String name, String iso) {
-    this.name = name;
-    this.iso = iso;
-  }
-}
-// + mapperele de la DB sunt in infra
+//
+//package victor.training.clean.infra.db.entity;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.Id;
+//import lombok.Data;
+//
+//@Entity
+//@Data
+//public class Country {
+//  @Id
+//  @GeneratedValue
+//  private long id;
+//  private String name;
+//  private String iso;
+//
+//  public Country() {
+//  }
+//
+//  public Country(String name, String iso) {
+//    this.name = name;
+//    this.iso = iso;
+//  }
+//}
+//// + mapperele de la DB sunt in infra
