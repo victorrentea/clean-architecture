ddfb2b55bdb195354937ddf9fbc253cbefcc2ac4
Victor Rentea <victorrentea@gmail.com>
2023-12-14 17:06:47 +0200

    PR reject bad names: leaking abstractions

--- clean-application/src/main/java/victor/training/clean/domain/service/ILdapUserApiAdapter.java
+++ clean-application/src/main/java/victor/training/clean/domain/service/ILdapUserApiAdapter.java
@@ -2,6 +2,7 @@ package victor.training.clean.domain.service;
 
 import victor.training.clean.domain.model.User;
 
+// Joel Spolski "leaking abstractions" CEO of stackoverflow.com
 public interface ILdapUserApiAdapter {
   // infra gargabe
   User fetchUserDetailsFromLdap(String userId);
