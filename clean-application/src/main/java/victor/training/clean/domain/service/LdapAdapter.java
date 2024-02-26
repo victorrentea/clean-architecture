c67e32f8998a4d145385ee0d990bc71a926be55b
Victor Rentea <victorrentea@gmail.com>
2024-02-26 15:51:46 +0200

    Introduced a dedicated Adapter class. PR review: you've put the adapter in the wrong package .

---
+++ clean-application/src/main/java/victor/training/clean/domain/service/LdapAdapter.java
@@ -0,0 +1,39 @@
+package victor.training.clean.domain.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import victor.training.clean.domain.model.User;
+import victor.training.clean.infra.LdapApi;
+import victor.training.clean.infra.LdapUserDto;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Slf4j
+@Component
+public class LdapAdapter {
+    private final LdapApi ldapApi;
+
+
+  public User lookupUser(String userId) { // BEWARE: NETWORK API CALL INVOLVED
+    // ⚠️ external DTO directly used in my app
+    List<LdapUserDto> dtoList = ldapApi.searchUsingGET(userId.toUpperCase(), null, null); // fName???
+
+    if (dtoList.size() != 1) {
+      throw new IllegalArgumentException("Search for uid='" + userId + "' returned too many results: " + dtoList);
+    }
+
+    LdapUserDto ldapUserDto = dtoList.get(0);
+    String fullName = ldapUserDto.getFname() + " " + ldapUserDto.getLname().toUpperCase();
+    String username = ldapUserDto.getUn();
+    if (username.startsWith("s")) {// eg 's12051' is a system user
+     username ="system"; // ⚠️ dirty hack
+   }
+    User user = new User(username, fullName, Optional.ofNullable(ldapUserDto.getWorkEmail()));
+    return user;
+  }
+
+}
--- clean-application/src/main/java/victor/training/clean/domain/service/NotificationService.java
+++ clean-application/src/main/java/victor/training/clean/domain/service/NotificationService.java
@@ -18,10 +18,10 @@ import java.util.Optional;
 @Service
 public class NotificationService {
   private final EmailSender emailSender;
-  private final LdapApi ldapApi;
+  private final LdapAdapter ldapAdapter;
 
   public void sendWelcomeEmail(Customer customer, String userId) {
-    User user = lookupUser(userId);
+    User user = ldapAdapter.lookupUser(userId);
 
     Email email = Email.builder()
         .from("noreply@cleanapp.com")
@@ -42,24 +42,6 @@ public class NotificationService {
     emailSender.sendEmail(email);
 
     customer.setCreatedByUsername(user.username());
-  }
-
-  private User lookupUser(String userId) { // BEWARE: NETWORK API CALL INVOLVED
-    // ⚠️ external DTO directly used in my app
-    List<LdapUserDto> dtoList = ldapApi.searchUsingGET(userId.toUpperCase(), null, null); // fName???
-
-    if (dtoList.size() != 1) {
-      throw new IllegalArgumentException("Search for uid='" + userId + "' returned too many results: " + dtoList);
-    }
-
-    LdapUserDto ldapUserDto = dtoList.get(0);
-    String fullName = ldapUserDto.getFname() + " " + ldapUserDto.getLname().toUpperCase();
-    String username = ldapUserDto.getUn();
-    if (username.startsWith("s")) {// eg 's12051' is a system user
-     username ="system"; // ⚠️ dirty hack
-   }
-    User user = new User(username, fullName, Optional.ofNullable(ldapUserDto.getWorkEmail()));
-    return user;
   }
 
 
