adfe6b891aa444d86b73051903aa883f4a2acf76
Victor Rentea <victorrentea@gmail.com>
2024-03-25 13:34:54 +0200

    extracted interaction into an Adapter

--- clean-application/src/main/java/victor/training/clean/domain/service/NotificationService.java
+++ clean-application/src/main/java/victor/training/clean/domain/service/NotificationService.java
@@ -7,22 +7,16 @@ import victor.training.clean.domain.model.Customer;
 import victor.training.clean.domain.model.Email;
 import victor.training.clean.domain.model.User;
 import victor.training.clean.infra.EmailSender;
-import victor.training.clean.infra.LdapApi;
-import victor.training.clean.infra.LdapUserDto;
-
-import java.util.List;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Slf4j
 @Service
 public class NotificationService {
   private final EmailSender emailSender;
-  private final LdapApi ldapApi;
-
+private final UserLdapAdapter userLdapAdapter;
   public void sendWelcomeEmail(Customer customer, String username) {
     // ⚠️ external DTO directly used in my app logic TODO convert it into a new dedicated Value Object
-    User user = fetchUser(username);
+    User user = userLdapAdapter.fetchUser(username);
 
     // ⚠️ data mapping mixed with my core domain logic TODO pull it earlier
 
@@ -47,21 +41,6 @@ public class NotificationService {
 
 //    normalize(ldapUserDto);
     customer.setCreatedByUsername(user.username());
-  }
-
-  private User fetchUser(String username) {
-    List<LdapUserDto> dtoList = ldapApi.searchUsingGET(username.toUpperCase(), null, null);
-
-    if (dtoList.size() != 1) {
-      throw new IllegalArgumentException("Search for uid='" + username + "' returned too many results: " + dtoList);
-    }
-
-    LdapUserDto ldapUserDto = dtoList.get(0);
-
-    return new User(
-        ldapUserDto.getUn().startsWith("s")? "system" : ldapUserDto.getUn(),
-        Optional.ofNullable(ldapUserDto.getWorkEmail()),
-        ldapUserDto.getFname() + " " + ldapUserDto.getLname().toUpperCase());
   }
 
 }
---
+++ clean-application/src/main/java/victor/training/clean/domain/service/UserLdapAdapter.java
@@ -0,0 +1,39 @@
+package victor.training.clean.domain.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import victor.training.clean.domain.model.User;
+import victor.training.clean.infra.LdapApi;
+import victor.training.clean.infra.LdapUserDto;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+
+// I call an external ugly API,
+// but I expose a NICE api to my caller (the rest of my logic)
+// what desing pattern is this?
+public class UserLdapAdapter {
+    private final LdapApi ldapApi;
+
+  public User fetchUser(String username) {
+    List<LdapUserDto> dtoList = ldapApi.searchUsingGET(username.toUpperCase(), null, null);
+
+    if (dtoList.size() != 1) {
+      throw new IllegalArgumentException("Search for uid='" + username + "' returned too many results: " + dtoList);
+    }
+
+    LdapUserDto ldapUserDto = dtoList.get(0);
+
+    String u = ldapUserDto.getUn().startsWith("s") ? "system" : ldapUserDto.getUn();
+    return new User(
+        u,
+        Optional.ofNullable(ldapUserDto.getWorkEmail()),
+        ldapUserDto.getFname() + " " + ldapUserDto.getLname().toUpperCase());
+  }
+
+}
