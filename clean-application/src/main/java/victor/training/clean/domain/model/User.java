0ca58f040cf65005725aea1a4e51a48dcdad7d96
Victor Rentea <victorrentea@gmail.com>
2024-04-04 17:47:25 +0200

    Refactor User domain model
    The User domain model has been refactored according to the value object principles to enhance immutability and tackle code smells. This improves code quality and supports further restructuring of the domain.

--- clean-application/src/main/java/victor/training/clean/domain/model/User.java
+++ clean-application/src/main/java/victor/training/clean/domain/model/User.java
@@ -1,7 +1,11 @@
 package victor.training.clean.domain.model;
 
 import java.util.Optional;
-
+// VALUE OBJECT = immutable small, lacking continuity change
 public record User(String username,
                    String fullName,
                    Optional<String> email) {}
+
+// Refactoring Level 1 = Code smells
+// Refactoring Level 2 = Restructuring Domain
+//
