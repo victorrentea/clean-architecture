906b0e76b2164940101b15518ec8426428741321
Victor Rentea <victorrentea@gmail.com>
2024-05-15 14:59:48 +0300

    am dus gunoiu de infra afara din domain
    PR reject by architect cu comentariul "introdu un Anti-Corruption Layer te rog"
    
    A ramas gunoiu in domeniu

---
+++ clean-application/src/main/java/victor/training/clean/domain/service/LdapService.java
@@ -0,0 +1,42 @@
+package victor.training.clean.domain.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import victor.training.clean.domain.model.User;
+import victor.training.clean.infra.LdapApi;
+import victor.training.clean.infra.LdapUserDto;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class LdapService {
+  private final LdapApi ldapApi;
+
+  public User fetchUser(String usernamePart) {
+    LdapUserDto ldapUserDto = fetchUserFromLdap(usernamePart);
+
+    String fullName = ldapUserDto.getFname() + " " + ldapUserDto.getLname().toUpperCase();
+
+    if (ldapUserDto.getUn().startsWith("s")) {
+      ldapUserDto.setUn("system"); // ⚠️ dirty hack: replace any system user with 'system'
+    }
+
+    return new User(ldapUserDto.getUn(),
+        fullName,
+        Optional.ofNullable(ldapUserDto.getWorkEmail()));
+  }
+
+  private LdapUserDto fetchUserFromLdap(String usernamePart) {
+    List<LdapUserDto> dtoList = ldapApi.searchUsingGET(usernamePart.toUpperCase(), null, null);
+
+    if (dtoList.size() != 1) {
+      throw new IllegalArgumentException("Search for username='" + usernamePart + "' did not return a single result: " + dtoList);
+    }
+
+    return dtoList.get(0);
+  }
+}
--- clean-application/src/main/java/victor/training/clean/domain/service/NotificationService.java
+++ clean-application/src/main/java/victor/training/clean/domain/service/NotificationService.java
@@ -18,11 +18,12 @@ import java.util.Optional;
 @Service
 public class NotificationService {
   private final EmailSender emailSender;
-  private final LdapApi ldapApi;
+  private final LdapService ldapService;
 
+  /////////
   // Core application logic, my Zen garden 🧘☯
   public void sendWelcomeEmail(Customer customer, String usernamePart) {
-    User user = fetchUser(usernamePart);
+    User user = ldapService.fetchUser(usernamePart);
     // gunoi de infra
     //----- linie- --------
     // biz curat
@@ -44,32 +45,8 @@ public class NotificationService {
     customer.setCreatedByUsername(user.username());
   }
 
-  private User fetchUser(String usernamePart) {
-    LdapUserDto ldapUserDto = fetchUserFromLdap(usernamePart);
-
-    String fullName = ldapUserDto.getFname() + " " + ldapUserDto.getLname().toUpperCase();
-
-    if (ldapUserDto.getUn().startsWith("s")) {
-      ldapUserDto.setUn("system"); // ⚠️ dirty hack: replace any system user with 'system'
-    }
-
-    return new User(ldapUserDto.getUn(),
-        fullName,
-        Optional.ofNullable(ldapUserDto.getWorkEmail()));
-  }
-
-  private LdapUserDto fetchUserFromLdap(String usernamePart) {
-    List<LdapUserDto> dtoList = ldapApi.searchUsingGET(usernamePart.toUpperCase(), null, null);
-
-    if (dtoList.size() != 1) {
-      throw new IllegalArgumentException("Search for username='" + usernamePart + "' did not return a single result: " + dtoList);
-    }
-
-    return dtoList.get(0);
-  }
-
   public void sendGoldBenefitsEmail(Customer customer, String usernamePart) {
-    User user = fetchUser(usernamePart);
+    User user = ldapService.fetchUser(usernamePart);
 
     String returnOrdersStr = customer.canReturnOrders() ? "You are allowed to return orders\n" : "";
 
