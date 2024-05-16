f9f576035dbe6921079af22f125b7e1faadb0f42
Victor Rentea <victorrentea@gmail.com>
2024-05-16 12:04:39 +0300

    debates

--- clean-application/src/main/java/victor/training/clean/domain/model/ShippingAddress.java
+++ clean-application/src/main/java/victor/training/clean/domain/model/ShippingAddress.java
@@ -2,11 +2,14 @@ package victor.training.clean.domain.model;
 
 import jakarta.persistence.Embeddable;
 
-// every class you write in Java should be record
-// unless an ORM @Entity or Spring @Service&friends
+// every class you write in Java should be record. immutable and low-shit code ratio
+// unless:
+// 1) an ORM @Entity or
+// 2) Spring @Service&friends(Aspecting doesn't work)
 @Embeddable
 // Value Object = small, immutable, lacking identity (no continuity of change)
 // the more VO you identify the DEEPER and more SEMANTIC RICH becomes your domain model
+// @Value (lombok)
 public record ShippingAddress(
     String city,
     String street,
