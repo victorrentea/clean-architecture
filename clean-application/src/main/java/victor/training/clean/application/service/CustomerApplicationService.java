3b8d29c6be02b2f7d9bd8cceb0130158971f7605
Victor Rentea <victorrentea@gmail.com>
2024-05-16 16:35:59 +0300

    extracted domain complexity in domain.service

--- clean-application/src/main/java/victor/training/clean/application/service/CustomerApplicationService.java
+++ clean-application/src/main/java/victor/training/clean/application/service/CustomerApplicationService.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import victor.training.clean.application.dto.CustomerDto;
 import victor.training.clean.application.ApplicationService;
-import victor.training.clean.domain.model.AnafResult;
 import victor.training.clean.domain.model.Country;
 import victor.training.clean.domain.model.Customer;
 import victor.training.clean.domain.repo.CustomerRepo;
+import victor.training.clean.domain.service.CustomerService;
+import victor.training.clean.domain.service.FiscalDetailsProvider;
 import victor.training.clean.domain.service.NotificationService;
-import victor.training.clean.infra.AnafClient;
 
 import static java.util.Objects.requireNonNull;
 
@@ -26,7 +26,7 @@ public class CustomerApplicationService {
   private final NotificationService notificationService;
   private final CustomerSearchQuery customerSearchQuery;
   private final InsuranceService insuranceService;
-  private final AnafClient anafClient;
+  private final FiscalDetailsProvider anafClient;
 
 //  public List<CustomerSearchResult> search(CustomerSearchCriteria searchCriteria) {
 //    return customerSearchQuery.search(searchCriteria);
@@ -43,35 +43,11 @@ public class CustomerApplicationService {
   public void register(CustomerDto dto) {
     Customer customer = dto.toEntity();
 
-
-    // business rule/validation
-    if (customerRepo.existsByEmail(customer.getEmail())) {
-      throw new IllegalArgumentException("A customer with this email is already registered!");
-      // throw new CleanException(CleanException.ErrorCode.DUPLICATED_CUSTOMER_EMAIL);
-    }
-
-    // enrich data from external API
-    if (customer.getLegalEntityCode() != null) {
-      if (customerRepo.existsByLegalEntityCode(customer.getLegalEntityCode().get())) {
-        throw new IllegalArgumentException("Company already registered");
-      }
-      AnafResult anafResult = anafClient.query(customer.getLegalEntityCode().get());
-      if (anafResult == null || !normalize(customer.getName()).equals(normalize(anafResult.getName()))) {
-        throw new IllegalArgumentException("Legal Entity not found!");
-      }
-      if (anafResult.isVatPayer()) {
-        customer.setDiscountedVat(true);
-      }
-    }
-    log.info("More Business Logic (imagine)");
-    log.info("More Business Logic (imagine)");
-    customerRepo.save(customer);
+    customerService.register(customer);
     notificationService.sendWelcomeEmail(customer, "FULL"); // userId from JWT token via SecuritContext
   }
+  private final CustomerService customerService;
 
-  private String normalize(String s) {
-    return s.toLowerCase().replace("\\s+", "");
-  }
 
   @Transactional
   public void update(long id, CustomerDto dto) { // TODO move to fine-grained Task-based Commands
