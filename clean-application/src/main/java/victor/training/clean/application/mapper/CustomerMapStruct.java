bcc41b99981cc43a5aff3b02ec01e80095e6b265
Victor Rentea <victorrentea@gmail.com>
2024-05-16 20:41:03 +0300

    mapstruct

--- clean-application/src/main/java/victor/training/clean/application/mapper/CustomerMapStruct.java
+++ clean-application/src/main/java/victor/training/clean/application/mapper/CustomerMapStruct.java
@@ -2,12 +2,22 @@ package victor.training.clean.application.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import victor.training.clean.domain.model.Customer;
+import org.mapstruct.Named;
 import victor.training.clean.application.dto.CustomerDto;
+import victor.training.clean.domain.model.Customer;
+
+import java.util.Optional;
 
 @Mapper(componentModel = "spring")
 public interface CustomerMapStruct {
-    @Mapping(target = "createdDateStr", source = "createdDate", dateFormat = "yyyy-MM-dd")
-    @Mapping(target = "countryId", source = "country.id")
-    CustomerDto toDto(Customer customer);
+  @Mapping(target = "createdDateStr", source = "createdDate", dateFormat = "yyyy-MM-dd")
+  @Mapping(target = "countryId", source = "country.id")
+  // needed if Customer#getLegalEntity returns Optional<>
+  // @Mapping(target = "legalEntityCode", source = "legalEntityCode", qualifiedByName = "unwrap") // OMG!!
+  CustomerDto toDto(Customer customer);
+
+  @Named("unwrap")
+  default <T> T unwrap(Optional<T> optional) {// OMG
+    return optional.orElse(null);
+  }
 }
