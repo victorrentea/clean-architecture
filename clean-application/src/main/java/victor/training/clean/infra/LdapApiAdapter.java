1f4f4dadfe30b0723dbee0036c54c95de1d6cb6e
Victor Rentea <victorrentea@gmail.com>
2024-06-03 14:52:24 +0300

    Depedency inversion implemented cu @Test pe Jenkins!!

--- clean-application/src/main/java/victor/training/clean/infra/LdapApiAdapter.java
+++ clean-application/src/main/java/victor/training/clean/infra/LdapApiAdapter.java
@@ -11,9 +11,10 @@ import java.util.Optional;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class LdapApiAdapter {
+public class LdapApiAdapter implements victor.training.clean.domain.service.ILdapApiAdapter {
   private final LdapApi ldapApi;
 
+  @Override
   public User fetchUserByUsername(String usernamePart) {
     // ⚠️ Scary, large external DTO TODO extract needed parts into a new dedicated Value Object
     LdapUserDto ldapUserDto = fetchUserFromLdap(usernamePart);
