8131191866541568280d0c6bfe60cbb754c57cfa
Victor Rentea <victorrentea@gmail.com>
2024-07-09 23:12:44 +0300

    #raise

--- clean-application/src/test/java/victor/training/clean/ArchitectureTest.java
+++ clean-application/src/test/java/victor/training/clean/ArchitectureTest.java
@@ -16,7 +16,7 @@ public class ArchitectureTest {
     private final JavaClasses allProjectClasses = new ClassFileImporter().importPackages("victor.training");
 
     @Test
-//    @Disabled
+//    @Disabled("I'll fix this later")
     // NOTE: In case you don't understand this test, contact me:
     // call:0800ARCHITECT or victorrentea@gmail.com (the anarchitect)
     public void domain_independent_of_infrastructure() {
