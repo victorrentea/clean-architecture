95877b4b495ad2ca47b117d276b532651b0ac493
Victor Rentea <victorrentea@gmail.com>
2024-07-24 22:06:40 +0300

    polish

--- clean-application/src/test/java/victor/training/clean/ArchitectureTest.java
+++ clean-application/src/test/java/victor/training/clean/ArchitectureTest.java
@@ -1,6 +1,6 @@
 package victor.training.clean;
 
-import com.tngtech.archunit.core.domain.*;
+import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -9,35 +9,38 @@ import victor.training.clean.utils.ParameterizedReturnTypeCondition;
 
 import static com.tngtech.archunit.base.DescribedPredicate.not;
 import static com.tngtech.archunit.core.domain.JavaClass.Predicates.resideInAPackage;
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.methods;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
 public class ArchitectureTest {
 
-    private final JavaClasses allProjectClasses = new ClassFileImporter().importPackages("victor.training");
+  private final JavaClasses allProjectClasses = new ClassFileImporter().importPackages("victor.training");
 
-    @Test
-    @Disabled
-    // NOTE: In case you don't understand this test, contact me:
-    // call:0800ARCHITECT or victorrentea@gmail.com (the anarchitect)
-    public void domain_independent_of_infrastructure() {
-        noClasses().that().resideInAPackage("..domain..")
-                .should().dependOnClassesThat().resideInAPackage("..infra..")
-                .check(allProjectClasses);
-    }
+  @Disabled
+  // NOTE: In case you don't understand this test, contact me:
+  // call:0800ARCHITECT or victorrentea@gmail.com (the anarchitect)
+  @Test
+  public void domain_independent_of_infrastructure() {
+    noClasses().that()
+          .resideInAPackage("..domain..")
+        .should().dependOnClassesThat()
+          .resideInAPackage("..infra..")
+        .check(allProjectClasses);
+  }
 
-    @Test
-    public void domain_independent_of_application() {
-        // TODO check that no classes in the domain pacakge depend on any classes in the application (eg DTOs)
-    }
+  @Test
+  public void domain_independent_of_application() {
+    // TODO check that no classes in the domain pacakge depend on any classes in the application (eg DTOs)
+  }
 
-    @Test
-    @Disabled
-    public void domain_not_leaked_via_controller_methods() {
-        methods().that().areMetaAnnotatedWith(RequestMapping.class)
-                .and().arePublic()
-                .should().haveRawReturnType(not(resideInAPackage("..domain..")))
-                .andShould(new ParameterizedReturnTypeCondition(not(resideInAPackage("..domain.."))))
-                .check(allProjectClasses);
-    }
+  @Test
+  @Disabled
+  public void domain_not_leaked_via_controller_methods() {
+    methods().that().areMetaAnnotatedWith(RequestMapping.class)
+        .and().arePublic()
+        .should().haveRawReturnType(not(resideInAPackage("..domain..")))
+        .andShould(new ParameterizedReturnTypeCondition(not(resideInAPackage("..domain.."))))
+        .check(allProjectClasses);
+  }
 
 }
