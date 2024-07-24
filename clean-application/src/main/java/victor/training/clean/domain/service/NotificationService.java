40fa14899493f4ee98fa50d7c25d551badbdbc18
Victor Rentea <victorrentea@gmail.com>
2024-07-24 14:21:56 +0300

    I extracted out of my core logic infra concerns in a separate class in the infra/ package

--- clean-application/src/main/java/victor/training/clean/domain/service/NotificationService.java
+++ clean-application/src/main/java/victor/training/clean/domain/service/NotificationService.java
@@ -8,6 +8,7 @@ import victor.training.clean.domain.model.Email;
 import victor.training.clean.domain.model.User;
 import victor.training.clean.infra.EmailSender;
 import victor.training.clean.infra.LdapApi;
+import victor.training.clean.infra.LdapStuff;
 import victor.training.clean.infra.LdapUserDto;
 
 import java.util.List;
@@ -18,21 +19,11 @@ import java.util.Optional;
 @Service
 public class NotificationService {
   private final EmailSender emailSender;
-  private final LdapApi ldapApi;
+  private final LdapStuff ldapStuff;
 
   // Core application logic, my Zen garden 🧘☯
   public void sendWelcomeEmail(Customer customer, String usernamePart) {
-    // ⚠️ Scary, large external DTO TODO extract needed parts into a new dedicated Value Object
-    LdapUserDto ldapUserDto = fetchUserFromLdap(usernamePart);
-
-    // ⚠️ Data mapping mixed with core logic TODO pull it earlier
-    String fullName = ldapUserDto.getFname() + " " + ldapUserDto.getLname().toUpperCase();
-
-    normalize(ldapUserDto);
-    User user = new User(ldapUserDto.getUn(), fullName, Optional.ofNullable(ldapUserDto.getWorkEmail()));
-    // infra sh*t
-    // --- line ----------------
-    // MY logic
+    User user = ldapStuff.retrieveUser(usernamePart);
 
     Email email = Email.builder()
         .from("noreply@cleanapp.com")
@@ -44,7 +35,7 @@ public class NotificationService {
 
 
     if (user.email().isPresent()) {
-      email.getCc().add(fullName + " <" + user.email().get() + ">");
+      email.getCc().add(user.fullName() + " <" + user.email().get() + ">");
     }
 
     emailSender.sendEmail(email);
@@ -52,21 +43,6 @@ public class NotificationService {
     customer.setCreatedByUsername(user.username());
   }
 
-  private LdapUserDto fetchUserFromLdap(String usernamePart) {
-    List<LdapUserDto> dtoList = ldapApi.searchUsingGET(usernamePart.toUpperCase(), null, null);
-
-    if (dtoList.size() != 1) {
-      throw new IllegalArgumentException("Search for username='" + usernamePart + "' did not return a single result: " + dtoList);
-    }
-
-    return dtoList.get(0);
-  }
-
-  private void normalize(LdapUserDto dto) {
-    if (dto.getUn().startsWith("s")) {
-      dto.setUn("system"); // ⚠️ dirty hack: replace any system user with 'system'
-    }
-  }
 
 
 
---
+++ clean-application/src/main/java/victor/training/clean/infra/LdapStuff.java
@@ -0,0 +1,45 @@
+package victor.training.clean.infra;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import victor.training.clean.domain.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class LdapStuff {
+  private final LdapApi ldapApi;
+  public User retrieveUser(String usernamePart) {
+    // ⚠️ Scary, large external DTO TODO extract needed parts into a new dedicated Value Object
+    LdapUserDto ldapUserDto = fetchUserFromLdap(usernamePart);
+
+    // ⚠️ Data mapping mixed with core logic TODO pull it earlier
+    String fullName = ldapUserDto.getFname() + " " + ldapUserDto.getLname().toUpperCase();
+
+    normalize(ldapUserDto);
+    User user = new User(ldapUserDto.getUn(), fullName, Optional.ofNullable(ldapUserDto.getWorkEmail()));
+    // infra sh*t
+    return user;
+  }
+
+  private LdapUserDto fetchUserFromLdap(String usernamePart) {
+    List<LdapUserDto> dtoList = ldapApi.searchUsingGET(usernamePart.toUpperCase(), null, null);
+
+    if (dtoList.size() != 1) {
+      throw new IllegalArgumentException("Search for username='" + usernamePart + "' did not return a single result: " + dtoList);
+    }
+
+    return dtoList.get(0);
+  }
+
+  private void normalize(LdapUserDto dto) {
+    if (dto.getUn().startsWith("s")) {
+      dto.setUn("system"); // ⚠️ dirty hack: replace any system user with 'system'
+    }
+  }
+
+}
