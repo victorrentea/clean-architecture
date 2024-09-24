f3ab6ca66b8b38e53138f185c0fbd0703c21947b
Victor Rentea <victorrentea@gmail.com>
2024-09-25 00:02:49 +0300

    draft using Guice wip

---
+++ clean-application/src/main/java/victor/training/clean/GuiceModule.java
@@ -0,0 +1,14 @@
+package victor.training.clean;
+
+import com.google.inject.AbstractModule;
+import victor.training.clean.domain.service.UserFetcher;
+import victor.training.clean.infra.LdapClient;
+
+//public class GuiceModule extends AbstractModule {
+//  @Override
+//  protected void configure() {
+//    // Bind interfaces to their implementations
+//    bind(UserFetcher.class).to(LdapClient.class);
+//    bind()
+//  }
+//}
