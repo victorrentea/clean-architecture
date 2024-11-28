c5237608294102e879ba4e3c66f16f4f8a759427
Victor Rentea <victorrentea@gmail.com>
2024-11-28 18:41:14 +0200

    open api freeze

--- clean-application/src/test/java/victor/training/clean/MyOpenAPIDidNotChangeTest.java
+++ clean-application/src/test/java/victor/training/clean/MyOpenAPIDidNotChangeTest.java
@@ -2,7 +2,6 @@ package victor.training.clean;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.openapitools.openapidiff.core.OpenApiCompare;
@@ -18,6 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Map;
 
+import static java.nio.charset.Charset.defaultCharset;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
@@ -27,38 +27,31 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 public class MyOpenAPIDidNotChangeTest {
   @Autowired
   MockMvc mockMvc;
-  ObjectMapper jackson = new ObjectMapper();
 
-  @Value("classpath:/my-existing-openapi.json") // extracted from deploy
+  @Value("classpath:/my-existing-openapi.json") // extracted yesterday
   Resource myExpectedOpenAPI;
 
   @Test
   void my_contract_did_not_change() throws Exception {
-    String actualOpenAPIJson = prettifyJsonString(
-            mockMvc.perform(get("/v3/api-docs"))
-                    .andReturn().getResponse().getContentAsString());
+    String currentOpenAPI = mockMvc.perform(get("/v3/api-docs"))
+        .andReturn().getResponse().getContentAsString();
 
-    String expectedOpenAPIJson = prettifyJsonString(
-            IOUtils.toString(myExpectedOpenAPI.getInputStream())
-                    .replace(":8080", "")); // hack the extracted port
+    String expectedOpenAPI = myExpectedOpenAPI.getContentAsString(defaultCharset())
+        .replace(":8080", ""); // ignore the port
 
+//    if (!OpenApiCompare.fromContents(expectedOpenAPI, currentOpenAPI).isCompatible()) {
+//      System.err.println(new MarkdownRender().render(diff));
 
-    System.out.println("New contract: " + actualOpenAPIJson);
-    ChangedOpenApi diff = OpenApiCompare.fromContents(expectedOpenAPIJson, actualOpenAPIJson);
-
-    if (!diff.isCompatible()) {
-      String render = new MarkdownRender().render(diff);
-      System.err.println(render);
-
-      assertThat(actualOpenAPIJson)
-          .describedAs("Exposed OpenAPI should not have changed")
-          .isEqualTo(expectedOpenAPIJson);
-    }
+    assertThat(prettify(currentOpenAPI))
+        .describedAs("Exposed OpenAPI should not have changed")
+        .isEqualTo(prettify(expectedOpenAPI));
+//    }
   }
 
-  private String prettifyJsonString(String rawJson) throws JsonProcessingException {
-      return StringUtils.isBlank(rawJson) ? rawJson :
-              jackson.writerWithDefaultPrettyPrinter().writeValueAsString(
-                      jackson.readValue(rawJson, Map.class));
+  private String prettify(String rawJson) throws JsonProcessingException {
+    if (StringUtils.isBlank(rawJson)) return rawJson;
+    ObjectMapper jackson = new ObjectMapper();
+    return jackson.writerWithDefaultPrettyPrinter().writeValueAsString(
+            jackson.readValue(rawJson, Map.class));
   }
 }
