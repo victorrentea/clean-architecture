b81038e1a74b6a92eaf7a302a44a2e3485190edf
Victor Rentea <victorrentea@gmail.com>
2024-11-28 15:04:03 +0200

    took the trash out of domain 💪 #raise
    PR reject: domain still depends on infra

---
+++ clean-application/src/main/java/victor/training/clean/infra/LdapApiAdapter.java
@@ -0,0 +1,35 @@
+package victor.training.clean.infra;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import victor.training.clean.domain.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class LdapApiAdapter {
+  private final LdapApi ldapApi;
+
+  public User fetchUser(String usernamePart) {
+    // fetch
+    List<LdapUserDto> dtoList = ldapApi.searchUsingGET(usernamePart.toUpperCase(), null, null);
+    if (dtoList.size() != 1) {
+      throw new IllegalArgumentException("Search for username='" + usernamePart + "' did not return a single result: " + dtoList);
+    }
+    LdapUserDto ldapUserDto = dtoList.get(0);
+    // map
+    String fullName = ldapUserDto.getFname() + " " + ldapUserDto.getLname().toUpperCase();
+    if (ldapUserDto.getUn().startsWith("s")) {
+      ldapUserDto.setUn("system"); // ⚠️ dirty hack: replace any system user with 'system'
+    }
+    return new User(
+        ldapUserDto.getUn(),
+        fullName,
+        Optional.ofNullable(ldapUserDto.getWorkEmail()));
+  }
+
+}
