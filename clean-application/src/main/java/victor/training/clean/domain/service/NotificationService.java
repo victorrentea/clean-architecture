2ddee1c3dc828626516bb8c56349ac79529beaa7
Victor Rentea <victorrentea@gmail.com>
2024-12-04 22:46:51 +0200

    Revert "extracted 💩 out of my core logic"
    This reverts commit 68ae02fd8d05d76f9a0492bc0800e5e8efd266fe.

--- clean-application/src/main/java/victor/training/clean/domain/service/NotificationService.java
+++ clean-application/src/main/java/victor/training/clean/domain/service/NotificationService.java
@@ -5,13 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import victor.training.clean.domain.model.Customer;
 import victor.training.clean.domain.model.Email;
-import victor.training.clean.domain.model.User;
 import victor.training.clean.infra.EmailSender;
 import victor.training.clean.infra.LdapApi;
 import victor.training.clean.infra.LdapUserDto;
 
 import java.util.List;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -22,42 +20,54 @@ public class NotificationService {
 
   // Core application logic, my Zen garden 🧘☯☮️
   public void sendWelcomeEmail(Customer customer, String usernamePart) {
-    User user = fetchUser(usernamePart);
-    // ------
+    // ⚠️ Scary, large external DTO TODO extract needed parts into a new dedicated Value Object
+    LdapUserDto ldapUserDto = fetchUserFromLdap(usernamePart);
+
+    // ⚠️ Data mapping mixed with core logic TODO pull it earlier
+    String fullName = ldapUserDto.getFname() + " " + ldapUserDto.getLname().toUpperCase();
+
     Email email = Email.builder()
         .from("noreply@cleanapp.com")
         .to(customer.getEmail())
         .subject("Welcome!")
-        .body("Dear " + customer.getName() + ", welcome! Sincerely, " + user.fullName())
+        .body("Dear " + customer.getName() + ", welcome! Sincerely, " + fullName)
         .build();
-    if (user.email().isPresent()) {
-      String contact = user.fullName() + " <" + user.email().get().toLowerCase() + ">";
+
+
+    // ⚠️ Unguarded nullable fields can cause NPE in other places TODO return Optional<> from getter
+    if (ldapUserDto.getWorkEmail() != null) {
+      // ⚠️ Logic repeated in other places TODO move logic to the new class
+      String contact = fullName + " <" + ldapUserDto.getWorkEmail().toLowerCase() + ">";
       email.getCc().add(contact);
     }
+
     emailSender.sendEmail(email);
-    customer.setCreatedByUsername(user.username());
+
+    // ⚠️ Swap this line with next one to cause a bug (=TEMPORAL COUPLING) TODO make immutable💚
+    normalize(ldapUserDto);
+
+    // ⚠️ 'un' = bad name TODO in my ubiquitous language 'un' means 'username'
+    customer.setCreatedByUsername(ldapUserDto.getUn());
   }
 
-  private User fetchUser(String usernamePart) {
-    // fetch
+  private LdapUserDto fetchUserFromLdap(String usernamePart) {
     List<LdapUserDto> dtoList = ldapApi.searchUsingGET(usernamePart.toUpperCase(), null, null);
+
     if (dtoList.size() != 1) {
       throw new IllegalArgumentException("Search for username='" + usernamePart + "' did not return a single result: " + dtoList);
     }
-    LdapUserDto ldapUserDto = dtoList.get(0);
-    // map
-    String fullName = ldapUserDto.getFname() + " " + ldapUserDto.getLname().toUpperCase();
+
+    return dtoList.get(0);
+  }
+
+  private void normalize(LdapUserDto ldapUserDto) {
     if (ldapUserDto.getUn().startsWith("s")) {
       ldapUserDto.setUn("system"); // ⚠️ dirty hack: replace any system user with 'system'
     }
-    return new User(
-        ldapUserDto.getUn(),
-        fullName,
-        Optional.ofNullable(ldapUserDto.getWorkEmail()));
   }
 
   public void sendGoldBenefitsEmail(Customer customer, String usernamePart) {
-    User user = fetchUser(usernamePart);
+    LdapUserDto userLdapDto = fetchUserFromLdap(usernamePart);
 
     boolean canReturnOrders = customer.canReturnOrders();
 
@@ -68,13 +78,12 @@ public class NotificationService {
         .to(customer.getEmail())
         .subject("Welcome to our Gold membership!")
         .body(returnOrdersStr +
-              "Yours sincerely, " + user.fullName())
+              "Yours sincerely, " + userLdapDto.getFname() + " " + userLdapDto.getLname().toUpperCase())
         .build();
 
-    if (user.email().isPresent()) {
-      String contact = user.fullName() + " <" + user.email().get().toLowerCase() + ">";
-      email.getCc().add(contact);
-    }
+    String contact = userLdapDto.getFname() + " " + userLdapDto.getLname().toUpperCase()
+               + " <" + userLdapDto.getWorkEmail().toLowerCase() + ">";
+    email.getCc().add(contact);
 
     emailSender.sendEmail(email);
   }
