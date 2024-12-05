a3f8f1fc04f5ebd67f9480d9ab9a40f81badbe61
Victor Rentea <victorrentea@gmail.com>
2024-12-05 14:37:59 +0200

    proudly extract external corruption from my methods.
    PR rejected: Anarchitect says: 💩 in domain package

---
+++ clean-application/src/main/java/victor/training/clean/domain/model/User.java
@@ -0,0 +1,17 @@
+package victor.training.clean.domain.model;
+
+import lombok.NonNull;
+
+import java.util.Optional;
+
+public record User(
+    @NonNull
+    String username,
+    @NonNull
+    String fullName,
+    Optional<String> email // onl;y place in Java allowed
+) {
+  public Optional<String> asContact() {
+    return email.map(e -> fullName + " <" + e + ">");
+  }
+}
