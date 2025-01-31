41a82bbb5b54f54a09de6d3e9351eb2ee1272d03
Victor Rentea <victorrentea@gmail.com>
2025-01-31 18:32:49 +0200

    Revert "moved out infra from domain core logic"
    This reverts commit c5a640b7edf0e6e8c6f7ac15fe93df1196c4a168.

--- clean-application/src/main/java/victor/training/clean/application/mapper/CustomerMapStructImpl.java
+++ clean-application/src/main/java/victor/training/clean/application/mapper/CustomerMapStructImpl.java
@@ -1,57 +1,57 @@
-package victor.training.clean.application.mapper;
-
-import java.time.format.DateTimeFormatter;
-import java.util.Optional;
-import javax.annotation.processing.Generated;
-
-import org.mapstruct.Named;
-import org.springframework.stereotype.Component;
-import victor.training.clean.application.dto.CustomerDto;
-import victor.training.clean.domain.model.Country;
-import victor.training.clean.domain.model.Customer;
-
-@Component
-public class CustomerMapStructImpl {
-
-    private final DateTimeFormatter dateTimeFormatter_yyyy_MM_dd_0159776256 = DateTimeFormatter.ofPattern( "yyyy-MM-dd" );
-
-    @org.mapstruct.Mapping(target = "createdDate", source = "createdDate", dateFormat = "yyyy-MM-dd")
-    public CustomerDto toDto(Customer customer) {
-        if ( customer == null ) {
-            return null;
-        }
-
-        CustomerDto.CustomerDtoBuilder customerDto = CustomerDto.builder();
-
-        if ( customer.getCreatedDate() != null ) {
-            customerDto.createdDate( dateTimeFormatter_yyyy_MM_dd_0159776256.format( customer.getCreatedDate() ) );
-        }
-        customerDto.countryId( customerCountryId( customer ) );
-        customerDto.legalEntityCode( unwrapOpt( customer.getLegalEntityCode() ) );
-        customerDto.id( customer.getId() );
-        customerDto.name( customer.getName() );
-        customerDto.email( customer.getEmail() );
-        customerDto.goldMemberRemovalReason( customer.getGoldMemberRemovalReason() );
-        customerDto.status( customer.getStatus() );
-        customerDto.discountedVat( customer.isDiscountedVat() );
-
-        return customerDto.build();
-    }
-
-    private Long customerCountryId(Customer customer) {
-        if ( customer == null ) {
-            return null;
-        }
-        Country country = customer.getCountry();
-        if ( country == null ) {
-            return null;
-        }
-        long id = country.getId();
-        return id;
-    }
-
-    @Named("unwrapOpt")
-    public <T> T unwrapOpt(Optional<T> optional) {// OMG
-      return optional.orElse(null);
-    }
-}
+//package victor.training.clean.application.mapper;
+//
+//import java.time.format.DateTimeFormatter;
+//import java.util.Optional;
+//import javax.annotation.processing.Generated;
+//
+//import org.mapstruct.Named;
+//import org.springframework.stereotype.Component;
+//import victor.training.clean.application.dto.CustomerDto;
+//import victor.training.clean.domain.model.Country;
+//import victor.training.clean.domain.model.Customer;
+//
+//@Component
+//public class CustomerMapStructImpl {
+//
+//    private final DateTimeFormatter dateTimeFormatter_yyyy_MM_dd_0159776256 = DateTimeFormatter.ofPattern( "yyyy-MM-dd" );
+//
+//    @org.mapstruct.Mapping(target = "createdDate", source = "createdDate", dateFormat = "yyyy-MM-dd")
+//    public CustomerDto toDto(Customer customer) {
+//        if ( customer == null ) {
+//            return null;
+//        }
+//
+//        CustomerDto.CustomerDtoBuilder customerDto = CustomerDto.builder();
+//
+//        if ( customer.getCreatedDate() != null ) {
+//            customerDto.createdDate( dateTimeFormatter_yyyy_MM_dd_0159776256.format( customer.getCreatedDate() ) );
+//        }
+//        customerDto.countryId( customerCountryId( customer ) );
+//        customerDto.legalEntityCode( unwrapOpt( customer.getLegalEntityCode() ) );
+//        customerDto.id( customer.getId() );
+//        customerDto.name( customer.getName() );
+//        customerDto.email( customer.getEmail() );
+//        customerDto.goldMemberRemovalReason( customer.getGoldMemberRemovalReason() );
+//        customerDto.status( customer.getStatus() );
+//        customerDto.discountedVat( customer.isDiscountedVat() );
+//
+//        return customerDto.build();
+//    }
+//
+//    private Long customerCountryId(Customer customer) {
+//        if ( customer == null ) {
+//            return null;
+//        }
+//        Country country = customer.getCountry();
+//        if ( country == null ) {
+//            return null;
+//        }
+//        long id = country.getId();
+//        return id;
+//    }
+//
+//    @Named("unwrapOpt")
+//    public <T> T unwrapOpt(Optional<T> optional) {// OMG
+//      return optional.orElse(null);
+//    }
+//}
