fddfe8818d81402910f2c27998f24e9c216ff5df
Victor Rentea <victorrentea@gmail.com>
2025-05-19 15:29:15 +0300

    bad arch

--- clean-application/src/test/java/victor/training/clean/LargeIntegrationTest.java
+++ clean-application/src/test/java/victor/training/clean/LargeIntegrationTest.java
@@ -18,8 +18,8 @@ import victor.training.clean.domain.model.Customer;
 import victor.training.clean.application.dto.CustomerDto;
 import victor.training.clean.application.dto.CustomerDto.CustomerDtoBuilder;
 import victor.training.clean.domain.repo.CountryRepo;
-import victor.training.clean.domain.service.EmailSender;
 import victor.training.clean.domain.repo.CustomerRepo;
+import victor.training.clean.infra.EmailSenderSMTP;
 
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -50,7 +50,7 @@ public class LargeIntegrationTest {
     @Autowired
     private CustomerRepo customerRepo;
     @MockBean
-    private EmailSender emailSender;
+    private EmailSenderSMTP emailSender;
 
     private Country country;
 
