1147f6f8acccc4c2fbb535607bb2fb4d25cf38a1
Victor Rentea <victorrentea@gmail.com>
2025-05-19 15:31:22 +0300

    current state of my arch

--- clean-application/src/test/java/victor/training/clean/ArchUnitProTest.java
+++ clean-application/src/test/java/victor/training/clean/ArchUnitProTest.java
@@ -7,8 +7,13 @@ import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
 import com.tngtech.archunit.library.freeze.FreezingArchRule;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @AnalyzeClasses(packages = "victor.training.clean",
     importOptions = DoNotIncludeTests.class)
@@ -16,10 +21,20 @@ public class ArchUnitProTest {
 
   @ArchTest
   void domain_model_independent_of_application(JavaClasses classes) {
-    ArchRuleDefinition.noClasses().that()
-        .resideInAPackage("..domain.model..")
+    FreezingArchRule.freeze(ArchRuleDefinition.noClasses().that()
+            .resideInAPackage("..domain...")
         .should().dependOnClassesThat()
-        .resideInAPackage("..application..")
+            .resideInAPackage("..application.."))
+        .check(classes);
+  }
+
+
+  @ArchTest // as per ADR-005
+  public void domain_independent_of_infrastructure(JavaClasses classes) {
+    FreezingArchRule.freeze(noClasses().that()
+            .resideInAPackage("..domain..")
+            .should().dependOnClassesThat()
+            .resideInAPackage("..infra.."))
         .check(classes);
   }
 
