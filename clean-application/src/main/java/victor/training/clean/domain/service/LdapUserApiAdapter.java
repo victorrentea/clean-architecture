68d39f76dbd18d4d72c73ecaf53576ec8354ff6b
Victor Rentea <victorrentea@gmail.com>
2025-05-28 14:41:36 +0300

    extracted infra 💩 to a separate class

---
+++ clean-application/src/main/java/victor/training/clean/domain/service/LdapUserApiAdapter.java
@@ -0,0 +1,50 @@
+package victor.training.clean.domain.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import victor.training.clean.domain.model.User;
+import victor.training.clean.infra.LdapApi;
+import victor.training.clean.infra.LdapUserDto;
+
+import java.util.List;
+import java.util.Optional;
+
+// "Adapter" design pattern acting here as an
+// Anti-Corruption Layer (ACL)
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class LdapUserApiAdapter {
+  private final LdapApi ldapApi;
+
+  public User fetchUser(String usernamePart) {
+    LdapUserDto ldapUserDto = fetchUserFromLdap(usernamePart);
+
+    String fullName = ldapUserDto.getFname() + " " + ldapUserDto.getLname().toUpperCase();
+
+    normalize(ldapUserDto);
+
+    return new User(
+        fullName,
+        Optional.ofNullable(ldapUserDto.getWorkEmail()),
+        ldapUserDto.getUn()
+    );
+  }
+
+  private LdapUserDto fetchUserFromLdap(String usernamePart) {
+    List<LdapUserDto> dtoList = ldapApi.searchUsingGET(usernamePart.toUpperCase(), null, null);
+
+    if (dtoList.size() != 1) {
+      throw new IllegalArgumentException("Search for username='" + usernamePart + "' did not return a single result: " + dtoList);
+    }
+
+    return dtoList.get(0);
+  }
+
+  private void normalize(LdapUserDto ldapUserDto) {
+    if (ldapUserDto.getUn().startsWith("s")) {
+      ldapUserDto.setUn("system"); // ⚠️ dirty hack: replace any system user with 'system'
+    }
+  }
+}
--- clean-application/src/main/java/victor/training/clean/domain/service/NotificationService.java
+++ clean-application/src/main/java/victor/training/clean/domain/service/NotificationService.java
@@ -5,17 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import victor.training.clean.domain.model.Customer;
 import victor.training.clean.domain.model.Email;
-import victor.training.clean.domain.model.User;
 import victor.training.clean.infra.EmailSender;
 import victor.training.clean.infra.LdapApi;
-import victor.training.clean.infra.LdapUserDto;
 
-import java.util.List;
-import java.util.Optional;
-
-class Adapter {
-
-}
 
 @RequiredArgsConstructor
 @Slf4j
@@ -23,22 +15,11 @@ class Adapter {
 public class NotificationService {
   private final EmailSender emailSender;
   private final LdapApi ldapApi;
+  private final LdapUserApiAdapter ldapUserApiAdapter;
 
   // Core application logic, my Zen garden 🧘☯☮️
   public void sendWelcomeEmail(Customer customer, String usernamePart) {
-    LdapUserDto ldapUserDto = fetchUserFromLdap(usernamePart);
-
-    String fullName = ldapUserDto.getFname() + " " + ldapUserDto.getLname().toUpperCase();
-
-    normalize(ldapUserDto);
-
-    User user = new User(
-        fullName,
-        Optional.ofNullable(ldapUserDto.getWorkEmail()),
-        ldapUserDto.getUn()
-    );
-    //-------- nothing from the evil CORRUPTED world outside should be below this line
-
+    var user = ldapUserApiAdapter.fetchUser(usernamePart);
     Email email = Email.builder()
         .from("noreply@cleanapp.com")
         .to(customer.getEmail())
@@ -56,21 +37,5 @@ public class NotificationService {
     customer.setCreatedByUsername(user.username());
   }
 
-  private LdapUserDto fetchUserFromLdap(String usernamePart) {
-    List<LdapUserDto> dtoList = ldapApi.searchUsingGET(usernamePart.toUpperCase(), null, null);
-
-    if (dtoList.size() != 1) {
-      throw new IllegalArgumentException("Search for username='" + usernamePart + "' did not return a single result: " + dtoList);
-    }
-
-    return dtoList.get(0);
-  }
-
-  private void normalize(LdapUserDto ldapUserDto) {
-    if (ldapUserDto.getUn().startsWith("s")) {
-      ldapUserDto.setUn("system"); // ⚠️ dirty hack: replace any system user with 'system'
-    }
-  }
-
-
 }
+
