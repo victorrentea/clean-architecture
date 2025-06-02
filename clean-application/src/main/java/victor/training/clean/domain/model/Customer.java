a8bab7a5e9c86f5c16fc410366424dde54dae0d0
Victor Rentea <victorrentea@gmail.com>
2025-06-02 12:31:54 +0300

    guarding valid states with methods replacing setters

--- clean-application/src/main/java/victor/training/clean/domain/model/Customer.java
+++ clean-application/src/main/java/victor/training/clean/domain/model/Customer.java
@@ -1,10 +1,16 @@
 package victor.training.clean.domain.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.AssertTrue;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.Objects;
 import java.util.Optional;
+
+import static lombok.AccessLevel.NONE;
 
 //region Reasons to avoid @Data on Domain Model
 // Avoid @Data on Domain Model because:
@@ -23,17 +29,25 @@ public class Customer {
   private String name;
   private String email;
 
+  @Setter(NONE)
+  private Status status = Status.DRAFT;
   // 🤔 Hmm... 3 fields with the same prefix. What TODO ?
 //  private String shippingAddressCity;
 //  private String shippingAddressStreet;
 //  private String shippingAddressZip;
   @Embedded
   private ShippingAddress shippingAddress;
+  @Setter(NONE)
+  private String validatedBy; // ⚠ Always not-null when status = VALIDATED or later
 
-  @Embeddable
-  // 👑 Value Object, a small object that represents a value in the domain.
-  // It is immutable and has no identity.
-  public record ShippingAddress(String city, String street, String zip) {}
+  //bits of logic based on my fields.
+  public boolean canReturnOrders() {
+    return goldMember || isPhysicalPerson();
+  }
+
+
+  // shouldn't I name it as "Address" to be more reuseable? (DRY) as a BillingAddress?
+  // billing address might have other required fields, or extra fields (e.g. VAT number)
 
   @ManyToOne
   private Country country;
@@ -51,24 +65,65 @@ public class Customer {
     return Optional.ofNullable(legalEntityCode);
   }
 
-  public enum Status {
-    DRAFT, VALIDATED, ACTIVE, DELETED
+  public boolean isPhysicalPerson() {
+    return legalEntityCode == null;
   }
-  private Status status;
-  private String validatedBy; // ⚠ Always not-null when status = VALIDATED or later
+
+  // guarding state mutations with methods that enforce invariants
+  public void validate(String username) {
+    if (status != Status.DRAFT) {
+      throw new IllegalStateException("Cannot validate a customer that is not in DRAFT status");
+    }
+    validatedBy = Objects.requireNonNull(username);
+    status = Status.VALIDATED;
+  }
+
+  public void activate() {
+    if (status != Status.VALIDATED) {
+      throw new IllegalStateException("Cannot activate a customer that is not in VALIDATED status");
+    }
+    status = Status.ACTIVE;
+  }
+
+//  @AssertTrue // opt1 validate at peristence time < most team prefer this
+//  public boolean hasValidatedBy() {
+//    if (status == Status.DRAFT) return true;
+//    return validatedBy != null;
+//  }
+
+  public void delete() {
+    if (status == Status.DELETED) {
+      throw new IllegalStateException("Cannot delete a customer that is already DELETED");
+    }
+    status = Status.DELETED;
+  }
+
+  public enum Status {
+    DRAFT, VALIDATED, ACTIVE, DELETED;
+  }
+
+  @Embeddable
+  // 👑 Value Object, a small object that represents a value in the domain.
+  // It is immutable and has no identity.
+  public record ShippingAddress(String city, String street, String zip) {}
+
+
+  // opt2: O-O-P: Customer's methods prevent object from being in an invalid state
+
 }
 
 //region Code in the project might [not] follow the rule
-//class SomeCode {
-//  public void correct(Customer draftCustomer) {
-//    draftCustomer.setStatus(Customer.Status.VALIDATED);
-//    draftCustomer.setValidatedBy("currentUser"); // from token/session..
-//  }
-//  public void incorrect(Customer draftCustomer) {
-//    draftCustomer.setStatus(Customer.Status.VALIDATED);
-//  }
-//  public void activate(Customer draftCustomer) {
-//    draftCustomer.setStatus(Customer.Status.ACTIVE);
-//  }
-//}
+class SomeCode {
+  public void correct(Customer draftCustomer) {
+    draftCustomer.validate("currentUser"); // from token/session..
+  }
+
+  public void incorrect(Customer draftCustomer) {
+    draftCustomer.validate("user");
+  }
+
+  public void activate(Customer draftCustomer) {
+    draftCustomer.activate();
+  }
+}
 //endregion
