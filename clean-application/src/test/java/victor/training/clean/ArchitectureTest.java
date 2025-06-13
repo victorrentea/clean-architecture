bf048a0080086c413062fc3fcd29a8c7efedede7
Victor Rentea <victorrentea@gmail.com>
2025-06-13 13:33:18 +0300

    un pic mai bine. mai aproape de ideal

--- clean-application/src/test/java/victor/training/clean/ArchitectureTest.java
+++ clean-application/src/test/java/victor/training/clean/ArchitectureTest.java
@@ -37,8 +37,8 @@ public class ArchitectureTest {
         .resideInAPackage("..infra..");
     List<String> failures = rule.evaluate(allProjectClasses).getFailureReport().getDetails();
 
-    int expectedFailureCount = 21; //  initial 😭
-//    int expectedFailureCount = 10; //  3 months later
+//    int expectedFailureCount = 21; //  initial 😭
+    int expectedFailureCount = 17; //  3 months later 🍺
 //    int expectedFailureCount = 0; // end 🍾
 
     assertEquals(expectedFailureCount, failures.size(), String.join("\n", failures));
