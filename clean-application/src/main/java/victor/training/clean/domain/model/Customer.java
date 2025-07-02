23fea27c34d7304c49644efbf35a32e3b99c2444
Victor Rentea <victorrentea@gmail.com>
2025-07-02 23:29:18 +0300

    end of day1

--- clean-application/src/main/java/victor/training/clean/domain/model/Customer.java
+++ clean-application/src/main/java/victor/training/clean/domain/model/Customer.java
@@ -1,10 +1,18 @@
 package victor.training.clean.domain.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.Objects;
 import java.util.Optional;
+
+import static java.util.Objects.requireNonNull;
+import static lombok.AccessLevel.NONE;
 
 //region Reasons to avoid @Data on Domain Model
 // Avoid @Data on Domain Model because:
@@ -13,6 +21,7 @@ import java.util.Optional;
 // 3) all setters/getters = no encapsulation⚠️
 //endregion
 
+//@HasValidatedByForBlaBlaBla #BAN!!
 @Data // = @Getter @Setter @ToString @EqualsAndHashCode (1)
 @Entity // ORM/JPA (2)
 // 👑 Domain Model Entity, the backbone of your core complexity.
@@ -20,12 +29,20 @@ public class Customer {
   @Id
   @GeneratedValue
   private Long id;
+  @NotNull
   private String name;
   private String email;
 
   // 🤔 Hmm... 3 fields with the same prefix. What TODO ?
   @Embedded // no ALTER TABLE is needed.
   private ShippingAddress shippingAddress;
+
+//  public boolean f(SomeService) { // NOGO
+//  public boolean f(ApiClient) { // NOGO
+//  public boolean f(SomeRepo) { // NOGO
+//  public boolean f(AnotherDomainModelOf70fields) { // NOGO
+//  public boolean f() { // 10 ifs
+  // all of these => stateless service
 
   public boolean canReturnOrders() { // biz rules
     return goldMember || isPerson();
@@ -62,21 +79,50 @@ public class Customer {
   public enum Status {
     DRAFT, VALIDATED, ACTIVE, DELETED
   }
-  private Status status;
-  private String validatedBy; // ⚠ Always not-null when status = VALIDATED or later
+  @Setter(NONE)
+  private Status status = Status.DRAFT;
+  @Setter(NONE)
+  private String validatedBy;
+
+  @AssertTrue
+  public boolean hasValidatedByForAnyStateAfterValidation() {
+    return status != Status.DRAFT || validatedBy != null;
+  }
+
+  public void delete() {
+    if (status == Status.DELETED) {
+      throw new IllegalStateException();
+    }
+    status=Status.DELETED;
+  }
+
+  public void validate(String username) {
+    requireNonNull(username, "username is mandatory");
+    if (status != Status.DRAFT) {
+      throw new IllegalStateException("Can validate a customer in DRAFT status");
+    }
+    this.status = Status.VALIDATED;
+    this.validatedBy = username;
+  }
+
+  public void activate() {
+    if (status != Status.VALIDATED) {
+      throw new IllegalStateException("Can activate a customer in VALIDATED status only");
+    }
+    this.status = Status.ACTIVE;
+  }
 }
 
 //region Code in the project might [not] follow the rule
-//class SomeCode {
-//  public void correct(Customer draftCustomer) {
-//    draftCustomer.setStatus(Customer.Status.VALIDATED);
-//    draftCustomer.setValidatedBy("currentUser"); // from token/session..
-//  }
-//  public void incorrect(Customer draftCustomer) {
-//    draftCustomer.setStatus(Customer.Status.VALIDATED);
-//  }
-//  public void activate(Customer draftCustomer) {
-//    draftCustomer.setStatus(Customer.Status.ACTIVE);
-//  }
-//}
+class SomeCode {
+  public void correct(Customer draftCustomer) {
+    draftCustomer.validate("curr");
+  }
+  public void incorrect(Customer draftCustomer) {
+    draftCustomer.validate("user");
+  }
+  public void activate(Customer draftCustomer) {
+    draftCustomer.activate();
+  }
+}
 //endregion
