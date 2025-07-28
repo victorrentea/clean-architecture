7603e17e3ef348bd180f7529deb2bd5480627b05
Victor Rentea <victorrentea@gmail.com>
2025-07-28 13:30:05 +0300

    2) moved bits of business logic into the data structure 😱@leonel

--- clean-application/src/main/java/victor/training/clean/domain/model/Customer.java
+++ clean-application/src/main/java/victor/training/clean/domain/model/Customer.java
@@ -1,9 +1,14 @@
 package victor.training.clean.domain.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 //region Reasons to avoid @Data on Domain Model
@@ -17,15 +22,29 @@ import java.util.Optional;
 //@Entity // ORM/JPA (2)
 // 👑 Domain Model Entity, the backbone of your core complexity.
 public class Customer {
+//  public Customer(String legalEntityCode,String email) {
+//    this.legalEntityCode = Objects.requireNonNull(legalEntityCode);
+//    this.email = Objects.requireNonNull(email);
+  ////    // to report all errors at once, collect them first
+  ////    List<String> errors = new ArrayList<>();
+  ////    if (legalEntityCode == null) {
+  ////      errors.add("Legal entity code is null");
+  ////    }
+  ////    if (email == null) {
+  ////      errors.add("Email is null");
+  ////    }
+//  }
   @Id
   @GeneratedValue
+
   private Long id;
   private String name;
-
+  @Email
   private String email; // yummy
 
   @Embedded
   private ShippingAddress shippingAddress;
+
 
   // explicitated a domain concept into its own type
   // Value Object (design pattern) = small, immutable, lacking PK
@@ -47,8 +66,17 @@ public class Customer {
   private boolean goldMember;
   private String goldMemberRemovalReason;
 
+  @NotNull
   private String legalEntityCode;
   private boolean discountedVat;
+
+  public boolean canReturnOrders() {
+    return goldMember || isPerson();
+  }
+
+  public boolean isPerson() {
+    return legalEntityCode == null;
+  }
 
   public Optional<String> getLegalEntityCode() {
     return Optional.ofNullable(legalEntityCode);
