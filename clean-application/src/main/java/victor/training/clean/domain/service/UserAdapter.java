1ceb746cd55096c4d5025d1aaf30cd9303d45588
Victor Rentea <victorrentea@gmail.com>
2025-07-28 16:18:15 +0300

    separated infra in  a separate class

---
+++ clean-application/src/main/java/victor/training/clean/domain/service/UserAdapter.java
@@ -0,0 +1,50 @@
+package victor.training.clean.domain.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import victor.training.clean.domain.model.User;
+import victor.training.clean.infra.LdapApi;
+import victor.training.clean.infra.LdapUserDto;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class UserAdapter {
+  private final LdapApi ldapApi;
+
+  // externally corrupted
+  public User fetchUser(String usernamePart) {
+    LdapUserDto ldapUserDto = fetchUserFromLdap(usernamePart);
+
+    String fullName = ldapUserDto.getFname() + " " + ldapUserDto.getLname().toUpperCase();
+
+    normalize(ldapUserDto);
+
+    return new User(fullName,
+        Optional.ofNullable(ldapUserDto.getWorkEmail()),
+        ldapUserDto.getUn());
+  }
+
+  // externally corrupted
+  private LdapUserDto fetchUserFromLdap(String usernamePart) {
+    List<LdapUserDto> dtoList = ldapApi.searchUsingGET(usernamePart.toUpperCase(), null, null);
+
+    if (dtoList.size() != 1) {
+      throw new IllegalArgumentException("Search for username='" + usernamePart + "' did not return a single result: " + dtoList);
+    }
+
+    return dtoList.get(0);
+  }
+
+  // externally corrupted
+  private void normalize(LdapUserDto ldapUserDto) {
+    if (ldapUserDto.getUn().startsWith("s")) {
+      ldapUserDto.setUn("system"); // ⚠️ dirty hack: replace any system user with 'system'
+    }
+  }
+
+}
