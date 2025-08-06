0d631d0930c340604f009770e48f33a3908d472a
Victor Rentea <victorrentea@gmail.com>
2025-08-06 17:00:41 +0300

    I reenabled the arch unit test and expected the current number of violations to the rule I target.

--- clean-application/src/test/java/victor/training/clean/ArchitectureTest.java
+++ clean-application/src/test/java/victor/training/clean/ArchitectureTest.java
@@ -20,7 +20,7 @@ import static com.tngtech.archunit.core.importer.ImportOption.Predefined.DO_NOT_
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Disabled("Fix this after I return from vacation")
+//@Disabled("Fix this after I return from vacation")
 // NOTE: In case you don't understand this test, contact me:
 // call:0800ARCHITECT or victorrentea@gmail.com (the anarchitect)
 public class ArchitectureTest {
@@ -37,9 +37,9 @@ public class ArchitectureTest {
         .resideInAPackage("..infra..");
     List<String> failures = rule.evaluate(allProjectClasses).getFailureReport().getDetails();
 
-//    int expectedFailureCount = 21; //  initial 😭
+    int expectedFailureCount = 13; //  initial 😭
 //    int expectedFailureCount = 10; //  3 months later
-    int expectedFailureCount = 0; // end 🍾
+//    int expectedFailureCount = 0; // end 🍾
 
     assertEquals(expectedFailureCount, failures.size(), String.join("\n", failures));
 
