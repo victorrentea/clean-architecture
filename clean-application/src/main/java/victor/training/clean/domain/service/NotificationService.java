3540a785c940a63405597bd31800cc1744ea1cae
Victor Rentea <victorrentea@gmail.com>
2025-12-15 16:45:31 +0200

    domain segregated from infrastructure

--- clean-application/src/main/java/victor/training/clean/domain/service/NotificationService.java
+++ clean-application/src/main/java/victor/training/clean/domain/service/NotificationService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 import victor.training.clean.domain.model.Customer;
 import victor.training.clean.domain.model.Email;
 import victor.training.clean.domain.repo.UserRepo;
-import victor.training.clean.infra.EmailSender;
 import victor.training.clean.domain.model.User;
 
 @RequiredArgsConstructor
