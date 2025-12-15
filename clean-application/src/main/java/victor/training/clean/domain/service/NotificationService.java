8b7c746bb2f623548f61bd6e69cb6ec809964672
Victor Rentea <victorrentea@gmail.com>
2025-12-15 16:12:42 +0200

    read code

--- clean-application/src/main/java/victor/training/clean/domain/service/NotificationService.java
+++ clean-application/src/main/java/victor/training/clean/domain/service/NotificationService.java
@@ -18,9 +18,7 @@ public class NotificationService {
   private final EmailSender emailSender;
   private final LdapApi ldapApi;
 
-  // ☮️ Core application logic - should be super clean 😇
   public void sendWelcomeEmail(Customer customer, String usernamePart) {
-    // ⚠️ Scary, large external DTO FIXME only using a small set of properties
     List<LdapUserDto> dtoList = ldapApi.searchUsingGET(usernamePart.toUpperCase(), null, null);
 
     if (dtoList.size() != 1) {
@@ -29,39 +27,35 @@ public class NotificationService {
 
     LdapUserDto ldapUserDto = dtoList.get(0);
 
-    // ⚠️ Data mapping mixed with core logic FIXME pull it earlier
     String fullName = ldapUserDto.getFname() + " " + ldapUserDto.getLname().toUpperCase();
 
     Email email = Email.builder()
-        .from("noreply@cleanapp.com")
-        .to(customer.getEmail())
-        .subject("Welcome!")
-        .body("""
-            Welcome %s!
-            Remember: you %s return orders.
-            Sincerely,
-            %s""".formatted(
-            customer.getName(),
-                customer.canReturnOrders() ? "can" : "cannot",
-            fullName))
-        .build();
+            .from("noreply@cleanapp.com")
+            .to(customer.getEmail())
+            .subject("Welcome!")
+            .body("""
+                    Welcome %s!
+                    Remember: you %s return orders.
+                    Sincerely,
+                    %s""".formatted(
+                    customer.getName(),
+                    customer.canReturnOrders() ? "can" : "cannot",
+                    fullName))
+            .build();
 
-
-    // ⚠️ Unguarded nullable fields can cause NPE in other places FIXME return Optional<> from getter
-    if (ldapUserDto.getWorkEmail() != null) { // what if forgotten?
-      // ⚠️ Logic only on User in other places FIXME move logic to the new class
+    // Q1: is it VALID be null? => here: YES
+    // Q2: return an Optional
+    if (ldapUserDto.getWorkEmail() != null) {
       String contact = fullName + " <" + ldapUserDto.getWorkEmail().toLowerCase() + ">";
       email.getCc().add(contact);
     }
 
     emailSender.sendEmail(email);
 
-    // ⚠️ Swap this line with next one to cause a bug (=TEMPORAL COUPLING) TODO make immutable💚
     normalize(ldapUserDto);
-
-    // ⚠️ 'un' = bad name FIXME in my Ubiquitous Language 'un' maps to 'username'
     customer.setCreatedByUsername(ldapUserDto.getUn());
   }
+
 
   private void normalize(LdapUserDto ldapUserDto) {
     if (ldapUserDto.getUn().startsWith("s")) {
