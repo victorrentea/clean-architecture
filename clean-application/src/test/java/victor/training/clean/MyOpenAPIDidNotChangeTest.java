6537689c6cbdb24902eb9ce05b5740533e99b280
Victor Rentea <victorrentea@gmail.com>
2026-01-29 00:37:19 +0200

    polish contract freeze test

--- clean-application/src/test/java/victor/training/clean/MyOpenAPIDidNotChangeTest.java
+++ clean-application/src/test/java/victor/training/clean/MyOpenAPIDidNotChangeTest.java
@@ -4,9 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
-import org.openapitools.openapidiff.core.OpenApiCompare;
-import org.openapitools.openapidiff.core.model.ChangedOpenApi;
-import org.openapitools.openapidiff.core.output.MarkdownRender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -28,27 +25,26 @@ public class MyOpenAPIDidNotChangeTest {
   @Autowired
   MockMvc mockMvc;
 
-  @Value("classpath:/my-existing-openapi.json") // extracted yesterday
-  Resource myExpectedOpenAPI;
+  @Value("classpath:/my-existing-openapi.json") // in src/test/resources
+  Resource contractFile;
 
   @Test
   void my_contract_did_not_change() throws Exception {
-    String currentOpenAPI = mockMvc.perform(get("/v3/api-docs"))
+    String contractExtractedFromCode = mockMvc.perform(get("/v3/api-docs"))
         .andReturn().getResponse().getContentAsString();
 
-    String expectedOpenAPI = myExpectedOpenAPI.getContentAsString(defaultCharset())
-        .replace(":8080", ""); // ignore the port
+    String contractSavedOnGit = contractFile.getContentAsString(defaultCharset())
+        .replace(":8080", ""); // ignoring the port
 
-//    if (!OpenApiCompare.fromContents(expectedOpenAPI, currentOpenAPI).isCompatible()) {
+//    if (!OpenApiCompare.fromContents(contractSavedOnGit, openApiExtractedFromCode).isCompatible())
 //      System.err.println(new MarkdownRender().render(diff));
 
-    assertThat(prettify(currentOpenAPI))
-        .describedAs("Exposed OpenAPI should not have changed")
-        .isEqualTo(prettify(expectedOpenAPI));
+    assertThat(prettifyJson(contractExtractedFromCode))
+        .isEqualTo(prettifyJson(contractSavedOnGit));
 //    }
   }
 
-  private String prettify(String rawJson) throws JsonProcessingException {
+  private String prettifyJson(String rawJson) throws JsonProcessingException {
     if (StringUtils.isBlank(rawJson)) return rawJson;
     ObjectMapper jackson = new ObjectMapper();
     return jackson.writerWithDefaultPrettyPrinter().writeValueAsString(
