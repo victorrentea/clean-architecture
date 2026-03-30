615d62dda54f9d1e2f53dc3a4cd7853a27312129
Victor Rentea <victorrentea@gmail.com>
2026-03-30 12:13:29 +0300

    arch unit caught me exposing internal domain model this is probably the biggest mistake you can do in computer science. We've been separating library contract from library implementation for 50 years.

--- clean-application/src/main/java/victor/training/clean/application/controller/CustomerLeakingController.java
+++ clean-application/src/main/java/victor/training/clean/application/controller/CustomerLeakingController.java
@@ -1,18 +1,20 @@
 package victor.training.clean.application.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import victor.training.clean.domain.model.Customer;
 import victor.training.clean.domain.repo.CustomerRepo;
 
-//@RestController
+@RestController
 @RequestMapping("customer-leaking")
 @RequiredArgsConstructor
 public class CustomerLeakingController {
    private final CustomerRepo customerRepo;
 
-   @GetMapping("{id}")
+   @GetMapping("{id}") // @GET
    public Customer findById(@PathVariable long id) {
       return customerRepo.findById(id).orElseThrow();
    }
