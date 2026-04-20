f3f021e96eb8d1e52dfaae5552158ee79dd4d1e5
Victor Rentea <victorrentea@gmail.com>
2026-04-20 12:54:09 +0300

    done +1 VO

--- clean-application/src/main/java/victor/training/clean/domain/model/Customer.java
+++ clean-application/src/main/java/victor/training/clean/domain/model/Customer.java
@@ -1,9 +1,9 @@
 package victor.training.clean.domain.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -16,8 +16,8 @@ import java.util.Optional;
 // 3) all setters/getters = no encapsulation⚠️
 //endregion
 
-@Entity // ORM (2)
 @Data // = @Getter + @Setter + @ToString + @EqualsAndHashCode (1)
+@Entity // ORM (2)
 // 💙 Domain Model Entity - backbone of your core complexity
 public class Customer {
   @Id
@@ -27,9 +27,45 @@ public class Customer {
   private String email;
 
   // 🤔 Hmm... 3 fields with the same prefix. What TODO ?
-  private String shippingAddressCity;
-  private String shippingAddressStreet;
-  private String shippingAddressZip;
+//  private String shippingAddressCity;
+//  private String shippingAddressStreet;
+//  private String shippingAddressZip;
+
+  @Valid
+  @Embedded //no  ALTER TABLE needed
+  private ShippingAddress shippingAddress;
+
+  //
+//  record Address(...) { 😊reusABLE, 🙁overgeneric?
+  @Embeddable
+  public record ShippingAddress(
+      // declarative
+      @NotBlank @Size(min = 3) String city,
+      @NotBlank String street,
+      String zip) {
+
+    // programatic self-validation
+    public ShippingAddress {
+      // same validations as @
+      //NotBlank and @Size(min=3) but in code, throwing IllegalArgumentException if not valid
+      if (city == null || city.isBlank() || city.length() < 3) {
+        throw new IllegalArgumentException("City must be at least 3 characters long");
+      }
+      if (street == null || street.isBlank()) {
+        throw new IllegalArgumentException("Street must not be blank");
+      }
+    }
+
+    // 😊specific
+  }
+//  record BillingAddress(String address, vatCode/cnp) {
+  // Premature Abstraction = The more generic you go up front,
+  // the more chances you take of the
+  // abstraction being wrong in the moment when you know the least about
+  // your problem.
+  // Design Humbly, specific
+
+  // extends is 😈Evil: eg: how can child constrain the parent field/behavior?
 
   @ManyToOne
   private Country country;
@@ -50,6 +86,7 @@ public class Customer {
   public enum Status {
     DRAFT, VALIDATED, ACTIVE, DELETED
   }
+
   private Status status;
   private String validatedBy; // ⚠ Always not-null when status = VALIDATED or later
 }
